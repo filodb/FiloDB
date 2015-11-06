7706fa6d1ad1df9db101464f28955e1f7aa03ff5
shrutig <shruti.999.999@gmail.com>
2015-11-06 15:37:27 +0530

    more refactoring

--- cassandra/src/main/scala/filodb.cassandra/metastore/DatasetTable.scala
+++ cassandra/src/main/scala/filodb.cassandra/metastore/DatasetTable.scala
@@ -33,10 +33,10 @@ with FiloCassandraConnector {
 
   override def fromRow(row: Row): Projection =
     Projection(projectionId(row),
-      name(row),
-      projectionSortColumn(row),
-      projectionReverse(row),
-      segmentSize = projectionSegmentSize(row))
+               name(row),
+               projectionSortColumn(row),
+               projectionReverse(row),
+               segmentSize = projectionSegmentSize(row))
 
   def initialize(): Future[Response] = create.ifNotExists.future().toResponse()
 
@@ -44,31 +44,31 @@ with FiloCassandraConnector {
 
   def insertProjection(projection: Projection): Future[Response] =
     insert.value(_.name, projection.dataset)
-      .value(_.projectionId, projection.id)
-      .value(_.projectionSortColumn, projection.sortColumn)
-      .value(_.projectionReverse, projection.reverse)
-      .value(_.projectionSegmentSize, projection.segmentSize)
-      .future.toResponse()
+          .value(_.projectionId, projection.id)
+          .value(_.projectionSortColumn, projection.sortColumn)
+          .value(_.projectionReverse, projection.reverse)
+          .value(_.projectionSegmentSize, projection.segmentSize)
+          .future.toResponse()
 
   def createNewDataset(dataset: Dataset): Future[Response] =
     (for { createResp <- insert.value(_.name, dataset.name)
-      .value(_.partitionColumn, dataset.partitionColumn)
-      .value(_.options, dataset.options.toString)
-      .ifNotExists.future().toResponse(AlreadyExists)
-           insertProj <- insertProjection(dataset.projections.head)
-           if (dataset.projections.nonEmpty) && createResp == Success }
-      yield { insertProj }).recover {
+                               .value(_.partitionColumn, dataset.partitionColumn)
+                               .value(_.options, dataset.options.toString)
+                               .ifNotExists.future().toResponse(AlreadyExists)
+          insertProj <- insertProjection(dataset.projections.head)
+             if (dataset.projections.nonEmpty) && createResp == Success }
+    yield { insertProj }).recover {
       case e: NoSuchElementException => throw new StorageEngineException(new Exception("Dataset already exists",e))
     }
 
   def getProjection(dataset: TableName, id: Int): Future[Projection] =
     select.where(_.name eqs dataset).and(_.projectionId eqs id)
-      .one().map(_.getOrElse(throw NotFoundError(s"Dataset $dataset")))
+          .one().map(_.getOrElse(throw NotFoundError(s"Dataset $dataset")))
 
   def getDataset(dataset: TableName): Future[Dataset] =
     for { proj <- getProjection(dataset, 0)
           Some((partCol, options)) <- select(_.partitionColumn, _.options).where(_.name eqs dataset).one() }
-      yield { Dataset(dataset, Seq(proj), partCol, DatasetOptions.fromString(options)) }
+    yield { Dataset(dataset, Seq(proj), partCol, DatasetOptions.fromString(options)) }
 
   // NOTE: CQL does not return any error if you DELETE FROM datasets WHERE name = ...
   def deleteDataset(name: String): Future[Response] =
