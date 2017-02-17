fcf10982511310d16b61107d8eda6dee1c7ac822
Evan Chan <velvia@apple.com>
2017-02-17 11:57:50 -0800

    Allow stress apps tablename and keyspace to be configurable via system properties

--- stress/src/main/scala/filodb.stress/RowReplaceStress.scala
+++ stress/src/main/scala/filodb.stress/RowReplaceStress.scala
@@ -32,9 +32,12 @@ object RowReplaceStress extends App {
     //scalastyle:on
   }
 
+  val datasetName = sys.props.getOrElse("stress.tablename", "nyc_taxi_replace")
+  val keyspaceName = sys.props.getOrElse("stress.keyspace", "filostress")
+
   // Setup SparkContext, etc.
   val conf = (new SparkConf).setAppName("RowReplaceStress")
-                            .set("spark.filodb.cassandra.keyspace", "filostress")
+                            .set("spark.filodb.cassandra.keyspace", keyspaceName)
                             .set("spark.sql.shuffle.partitions", "4")
                             .set("spark.scheduler.mode", "FAIR")
   val sc = new SparkContext(conf)
@@ -68,7 +71,6 @@ object RowReplaceStress extends App {
   val csvLines = csvDF.count()
   val injectedLines = injectedDF.count()
 
-  val datasetName = "nyc_taxi_replace"
   val ingestMillis = Perftools.timeMillis {
     puts("Starting batch ingestion...")
     injectedDF.sort($"medallion").write.format("filodb.spark").
