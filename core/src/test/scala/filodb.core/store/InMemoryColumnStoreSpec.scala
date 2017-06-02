48ed57e673c00745b98b2f483b62a9833e2a959a
Helena Edelson <helenaedelson@apple.com>
2017-06-02 14:49:59 -0700

    Muted test to be EOL'd.

--- core/src/test/scala/filodb.core/store/InMemoryColumnStoreSpec.scala
+++ core/src/test/scala/filodb.core/store/InMemoryColumnStoreSpec.scala
@@ -1,5 +1,8 @@
 package filodb.core.store
 
+import org.scalatest.DoNotDiscover
+
+@DoNotDiscover
 class InMemoryColumnStoreSpec extends ColumnStoreSpec {
   import scala.concurrent.ExecutionContext.Implicits.global
   val colStore = new InMemoryColumnStore(global)
