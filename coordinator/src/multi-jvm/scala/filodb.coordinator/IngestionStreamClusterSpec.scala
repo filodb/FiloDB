f313f15286b11a3c98f9b4b1449a08751ccf3343
Evan Chan <velvia@apple.com>
2017-07-11 16:33:53 -0700

    IngestionStreamClusterSpec fully passes, does distributed querying
    - Also, enable separate log files for each JVM
    
    This is a great milestone, have a repeatable multi-JVM test that tests sharding and distributed querying

--- coordinator/src/multi-jvm/scala/filodb.coordinator/IngestionStreamClusterSpec.scala
+++ coordinator/src/multi-jvm/scala/filodb.coordinator/IngestionStreamClusterSpec.scala
@@ -31,7 +31,8 @@ object IngestionStreamClusterSpecConfig extends MultiNodeConfig {
   commonConfig(globalConfig)
 }
 
-// A multi-JVM IngestionStream spec to test out sending to multiple nodes
+// A multi-JVM IngestionStream spec to test out routing to multiple nodes
+// and distributed querying across nodes
 abstract class IngestionStreamClusterSpec extends MultiNodeSpec(IngestionStreamClusterSpecConfig)
   with FunSpecLike with Matchers with BeforeAndAfterAll
   with CoordinatorSetupWithFactory
@@ -63,6 +64,7 @@ abstract class IngestionStreamClusterSpec extends MultiNodeSpec(IngestionStreamC
   schema.foreach { col => metaStore.newColumn(col, ref2).futureValue should equal (Success) }
 
   var clusterActor: ActorRef = _
+  var mapper: ShardMapper = _
 
   val sampleReader = new java.io.InputStreamReader(getClass.getResourceAsStream("/GDELT-sample-test.csv"))
   val headerCols = CsvStream.getHeaderColumns(sampleReader)
@@ -84,34 +86,37 @@ abstract class IngestionStreamClusterSpec extends MultiNodeSpec(IngestionStreamC
                              headerCols,
                              DatasetResourceSpec(4, 2), noOpSource)
       clusterActor ! msg
-      expectMsg(DatasetVerified)
+      // It takes a _really_ long time for the cluster actor singleton to start.
+      expectMsg(30.seconds.dilated, DatasetVerified)
+
+      // We have to subscribe and get our own copy of the ShardMap so that routeToShards can route
+      clusterActor ! SubscribeShardUpdates(ref2)
+      expectMsgPF(3.seconds.dilated) {
+        case ShardMapUpdate(ref2, newMap) => mapper = newMap
+      }
     }
     enterBarrier("dataset-setup-done")
   }
 
-  private def getColStoreRows(): Int = {
-    val paramSet = columnStore.getScanSplits(ref2, 1)
-    val rowIt = columnStore.scanRows(proj2, schema, 0, FilteredPartitionScan(paramSet.head))
-    rowIt.toSeq.length
-  }
-
   import IngestionStream._
+  import QueryCommands._
 
   /**
-   * Only one node is going to read the CSV, but we will get counts from both nodes
+   * Only one node is going to read the CSV, but we will get counts from both nodes and all shards
    */
-  ignore("should start ingestion and route rows to the right node") {
+  it("should start ingestion, route to shards, and do distributed querying") {
     runOn(first) {
       // TODO: replace with waiting for node ready message
-      Thread sleep 1000
+      Thread sleep 2000
 
       val config = ConfigFactory.parseString(s"""header = true
                                              batch-size = 10
                                              resource = "/GDELT-sample-test.csv"
                                              """)
-      val stream = (new CsvStreamFactory).create(config, projection6, 0)
-      val protocolActor = system.actorOf(IngestProtocol.props(clusterActor, projection6.datasetRef))
-      stream.routeToShards(new ShardMapper(1), projection6, protocolActor)
+      val stream = (new CsvStreamFactory).create(config, proj2, 0)
+      val protocolActor = system.actorOf(IngestProtocol.props(clusterActor, ref2))
+      // Now, route records to all different shards and nodes across cluster
+      stream.routeToShards(mapper, proj2, protocolActor)
 
       // TODO: find a way to wait for ingestion to be done via subscribing to node events
       Thread sleep 3000
@@ -119,20 +124,14 @@ abstract class IngestionStreamClusterSpec extends MultiNodeSpec(IngestionStreamC
 
     enterBarrier("ingestion-done")
 
-    runOn(second) {
-      val numRows = getColStoreRows()
-      // Find the test actor of the first node and send our results to it
-      val node1testActor = system.actorSelection(node(first) / "system" / "testActor1")
-      logger.debug(s"Node2  numRows = $numRows   node1testActor = $node1testActor")
-      node1testActor ! numRows
-    }
-
-    runOn(first) {
-      val numRowsFirst = getColStoreRows()
-      logger.debug(s"Node1   numRows = $numRowsFirst")
-      val numRowsSecond = expectMsgPF(5.seconds.dilated) { case i: Int => i }
-      (numRowsFirst + numRowsSecond) should equal (99)
-    }
+    // Both nodes can execute a distributed query to all shards and should get back the same answer
+    // Count all the records in every partition in every shard
+    // counting a non-partition column... can't count a partition column yet
+    val query = AggregateQuery(ref2, 0, QueryArgs("count", Seq("MonthYear")), FilteredPartitionQuery(Nil))
+    coordinatorActor ! query
+    val answer = expectMsgClass(classOf[AggregateResponse[Int]])
+    answer.elementClass should equal (classOf[Int])
+    answer.elements should equal (Array(99))
   }
 }
 
