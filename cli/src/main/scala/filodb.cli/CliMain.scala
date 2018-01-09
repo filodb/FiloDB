85e531ddaaf11a966f9ebf4c06e0d04646496037
Evan Chan <evan_chan@apple.com>
2018-01-08 16:58:15 -0800

    feat(cli): Add filo-cli clearMeta command to clear all metadata (#108)

--- cli/src/main/scala/filodb.cli/CliMain.scala
+++ cli/src/main/scala/filodb.cli/CliMain.scala
@@ -56,11 +56,12 @@ object CliMain extends ArgMain[Arguments] with CsvImportExport with FilodbCluste
 
   def printHelp(): Unit = {
     println("filo-cli help:")
-    println("  commands: init create importcsv list truncate")
+    println("  commands: init create importcsv list truncate clearMetadata")
     println("  dataColumns/partitionColumns: <colName1>:<type1>,<colName2>:<type2>,... ")
     println("  types:  int,long,double,string,bitmap,ts,map")
     println("  common options:  --dataset --database")
     println("  OR:  --select col1, col2  [--limit <n>]  [--outfile /tmp/out.csv]")
+    println("\n  NOTE: truncate and clearMetadata should NOT be used while FiloDB instances are up\n")
     println("\nStandalone client commands:")
     println("  --host <hostname/IP> [--port ...] --command indexnames --dataset <dataset>")
     println("  --host <hostname/IP> [--port ...] --command indexvalues --indexname <index> --dataset <dataset>")
@@ -89,6 +90,12 @@ object CliMain extends ArgMain[Arguments] with CsvImportExport with FilodbCluste
         case Some("init") =>
           println("Initializing FiloDB Admin keyspace and tables...")
           parse(metaStore.initialize(), timeout) {
+            case Success =>   println("Succeeded.")
+          }
+
+        case Some("clearMetadata") =>
+          println("WARNING!  Clearing all FiloDB metadata, checkpoints, ingestion configs...")
+          parse(metaStore.clearAllData(), timeout) {
             case Success =>   println("Succeeded.")
           }
 
