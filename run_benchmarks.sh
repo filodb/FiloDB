8e52e975776e4339cd70ac9e1ad32d138c869a15
Jackson Thamaran Bala <jackson_paul@apple.com>
2018-06-29 20:37:06 -0700

    feat(jmh): Update benchmarks script to add IngestionBenchmark (#220)
    * feat(jmh): Update benchmarks script to add IngestionBenchmark
    
    * Update benchmark script option

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-sbt "jmh/jmh:run -i 15 -wi 10 -f3 -jvmArgsAppend -XX:MaxInlineLevel=20 \
+sbt "jmh/jmh:run -rf json -i 15 -wi 10 -f3 -jvmArgsAppend -XX:MaxInlineLevel=20 \
  -jvmArgsAppend -Xmx4g -jvmArgsAppend -XX:MaxInlineSize=99 \
- filodb.jmh.QueryInMemoryBenchmark"
-
-sbt "jmh/jmh:run -i 15 -wi 10 -f3 -jvmArgsAppend -XX:MaxInlineLevel=20 \
- -jvmArgsAppend -Xmx4g -jvmArgsAppend -XX:MaxInlineSize=99 \
- filodb.jmh.IngestionBenchmark"
+ filodb.jmh.QueryInMemoryBenchmark filodb.jmh.IngestionBenchmark"
