93705ea5e0e84cd819b14fba4fcaa55ebee900f6
Vish Ramachandran <vish.ramachandran@gmail.com>
2018-11-07 15:18:31 -0800

    bug(query): Release ChunkMap locks on acquisition thread (#173)
    * Releasing locks on Task's onFinish/Error event may run on different thread than the iteration. This fixes that bug and places the release as soon as SerializableRangeVector is constructed.
    * Lock is held for a much less duration now.
    * Warning message when there are lingering locks before a query thread starts acquiring locks.

--- core/src/main/scala/filodb.core/query/RangeVector.scala
+++ core/src/main/scala/filodb.core/query/RangeVector.scala
@@ -1,5 +1,6 @@
 package filodb.core.query
 
+import com.typesafe.scalalogging.StrictLogging
 import kamon.Kamon
 import org.joda.time.DateTime
 
@@ -9,6 +10,7 @@ import filodb.core.metadata.Column
 import filodb.core.metadata.Column.ColumnType._
 import filodb.core.store.{ChunkInfoRowReader, ChunkScanMethod, ReadablePartition}
 import filodb.memory.{MemFactory, UTF8StringMedium}
+import filodb.memory.data.OffheapLFSortedIDMap
 import filodb.memory.format.{RowReader, ZeroCopyUTF8String => UTF8Str}
 
 /**
@@ -143,7 +145,7 @@ final class SerializableRangeVector(val key: RangeVectorKey,
     containers.toIterator.flatMap(_.iterate(schema)).drop(startRecordNo).take(numRows)
 }
 
-object SerializableRangeVector {
+object SerializableRangeVector extends StrictLogging {
   import filodb.core._
 
   val queryResultBytes = Kamon.histogram("query-engine-result-bytes")
@@ -161,9 +163,20 @@ object SerializableRangeVector {
     var numRows = 0
     val oldContainerOpt = builder.currentContainer
     val startRecordNo = oldContainerOpt.map(_.countRecords).getOrElse(0)
-    rv.rows.take(limit).foreach { row =>
-      numRows += 1
-      builder.addFromReader(row)
+    try {
+      // validate no locks are held by the thread. If there are, quite possible a lock acquire or release bug exists
+      val numLocksReleased = OffheapLFSortedIDMap.releaseAllSharedLocks()
+      if (numLocksReleased > 0) {
+        logger.warn(s"Number of locks before a query iterator consumption was non-zero. " +
+          s"This is indicative of a possible bug.")
+      }
+      rv.rows.take(limit).foreach { row =>
+        numRows += 1
+        builder.addFromReader(row)
+      }
+    } finally {
+      // When the query is done, clean up lingering shared locks caused by iterator limit.
+      OffheapLFSortedIDMap.releaseAllSharedLocks()
     }
     // If there weren't containers before, then take all of them.  If there were, discard earlier ones, just
     // start with the most recent one we started adding to
