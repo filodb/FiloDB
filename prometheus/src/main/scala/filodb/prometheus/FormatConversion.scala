0b4ebdd275ecfa14012295988cea62ec6bbe62b0
Shaik Sher Ali <sher_shaik@apple.com>
2019-01-29 17:45:49 -0800

    fix: revert unwanted commit

--- prometheus/src/main/scala/filodb/prometheus/FormatConversion.scala
+++ prometheus/src/main/scala/filodb/prometheus/FormatConversion.scala
@@ -9,7 +9,7 @@ import filodb.core.metadata.{Dataset, DatasetOptions}
  */
 object FormatConversion {
   // An official Prometheus-format Dataset object with a single timestamp and value
-  val dataset = Dataset("timeseries", Seq("tags:map"), Seq("timestamp:ts", "value:double"))
+  val dataset = Dataset("prometheus", Seq("tags:map"), Seq("timestamp:ts", "value:double"))
                   .copy(options = DatasetOptions(Seq("__name__", "_ns"),
                     "__name__", "value", Map("__name__" -> Seq("_bucket", "_count", "_sum")), Seq("le"),
                     Map("exporter" -> "_ns", "job" -> "_ns")))
