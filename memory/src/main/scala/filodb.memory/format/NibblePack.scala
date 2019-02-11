534d47c24d7b8b8c5e7fc0bdb843cbfdb2896118
Evan Chan <velvia@apple.com>
2019-02-11 10:36:35 -0800

    Changes to allow NibblePack code to read from an offset in input array

--- memory/src/main/scala/filodb.memory/format/NibblePack.scala
+++ memory/src/main/scala/filodb.memory/format/NibblePack.scala
@@ -11,24 +11,28 @@ import scalaxy.loops._
 object NibblePack {
   /**
    * Packs 8 input values into a buffer. Returns # of bytes encoded.
+   * @param offset the offset into the input vector containing the data to pack
+   * @param buf the UnsafeBuffer at which to write, offset 0 must be the position to write.
+   *
+   * @return the ending UnsafeBuffer position
    */
-  def pack8(input: Array[Long], buf: UnsafeBuffer): Int = {
+  def pack8(input: Array[Long], offset: Int, buf: UnsafeBuffer): Int = {
     var bufpos = 0
-    require(input.size >= 8)
+    require(input.size >= (offset + 8))
 
     var bitmask = 0
     // Figure out which words are nonzero, pack bitmask
-    for { i <- 0 until 8 optimized } {
+    for { i <- offset until offset + 8 optimized } {
       if (input(i) != 0) bitmask |= 1 << i
     }
-    buf.putByte(0, bitmask.toByte)
+    buf.putByte(bufpos + 0, bitmask.toByte)
     bufpos += 1
 
     if (bitmask != 0) {
       // figure out min # of nibbles to represent nonzero words
       var minLeadingZeros = 64
       var minTrailingZeros = 64
-      for { i <- 0 until 8 optimized } {
+      for { i <- offset until offset + 8 optimized } {
         minLeadingZeros = Math.min(minLeadingZeros, java.lang.Long.numberOfLeadingZeros(input(i)))
         minTrailingZeros = Math.min(minTrailingZeros, java.lang.Long.numberOfTrailingZeros(input(i)))
       }
@@ -36,14 +40,14 @@ object NibblePack {
       val trailingNibbles = minTrailingZeros / 4
       val numNibbles = 16 - (minLeadingZeros / 4) - trailingNibbles
       val nibbleWord = ((numNibbles << 4) | trailingNibbles)
-      buf.putByte(1, nibbleWord.toByte)
+      buf.putByte(bufpos + 1, nibbleWord.toByte)
       bufpos += 1
 
       // Decide which packer to use
       bufpos = if (numNibbles % 2 == 0) {
-        packToEvenNibbles(input, buf, numNibbles, trailingNibbles)
+        packToEvenNibbles(input, offset, buf, numNibbles, trailingNibbles)
       } else {
-        packToOddNibbles1(input, buf, numNibbles, trailingNibbles)
+        packToOddNibbles1(input, offset, buf, numNibbles, trailingNibbles)
       }
     }
 
@@ -51,11 +55,12 @@ object NibblePack {
   }
 
   // Retuerns the final bufpos
-  def packToEvenNibbles(inputs: Array[Long], buf: UnsafeBuffer, numNibbles: Int, trailingZeroNibbles: Int): Int = {
+  def packToEvenNibbles(inputs: Array[Long], offset: Int,
+                        buf: UnsafeBuffer, numNibbles: Int, trailingZeroNibbles: Int): Int = {
     var bufpos = 2
     val shift = trailingZeroNibbles * 4
     val numBytesEach = numNibbles / 2
-    for { i <- 0 until 8 optimized } {
+    for { i <- offset until offset + 8 optimized } {
       if (inputs(i) != 0) {
         buf.putLong(bufpos, inputs(i) >> shift, ByteOrder.LITTLE_ENDIAN)
         bufpos += numBytesEach
@@ -66,22 +71,23 @@ object NibblePack {
 
   // import scala.util.control.Breaks._
 
-  def packToOddNibbles1(inputs: Array[Long], buf: UnsafeBuffer, numNibbles: Int, trailingZeroNibbles: Int): Int = {
+  def packToOddNibbles1(inputs: Array[Long], offset: Int,
+                        buf: UnsafeBuffer, numNibbles: Int, trailingZeroNibbles: Int): Int = {
     val shift = trailingZeroNibbles * 4
     var i = 0
     var bufpos = 2
     while (i < 8) {
-        while (i < 8 && inputs(i) == 0) i += 1
+        while (i < 8 && inputs(offset + i) == 0) i += 1
         if (i < 8) {
           // if nonzero, shift first value into place
-          var packedword = inputs(i) >> shift
+          var packedword = inputs(offset + i) >> shift
           var bytespacked = (numNibbles + 1) / 2
 
           // find second value, shift into upper place
           i += 1
-          while (i < 8 && inputs(i) == 0) i += 1
+          while (i < 8 && inputs(offset + i) == 0) i += 1
           if (i < 8) {
-            packedword |= (inputs(i) >> shift) << (numNibbles * 4)
+            packedword |= (inputs(offset + i) >> shift) << (numNibbles * 4)
             bytespacked = numNibbles
           }
           // write out both values together
