bac9d70cdc8a61c40a42500cb69eb5d6025a0048
Brian O'Neill <broneill@apple.com>
2019-02-12 16:08:22 -0800

    Revert minor whitespace change.

--- filodb-dev-start.sh
+++ filodb-dev-start.sh
@@ -40,7 +40,7 @@ if [ ! -f standalone/target/scala-2.11/standalone-assembly-*-SNAPSHOT.jar ]; the
 fi
 
 echo "Starting FiloDB standalone server..."
-java -Xmx4G $PORTS_ARG -Dconfig.file=$CONFIG -DlogSuffix=$LOG_SUFFIX -cp standalone/target/scala-2.11/standalone-assembly-*-SNAPSHOT.jar filodb.standalone.FiloServer &
+java -Xmx4G $PORTS_ARG -Dconfig.file=$CONFIG -DlogSuffix=$LOG_SUFFIX -cp standalone/target/scala-2.11/standalone-assembly-*-SNAPSHOT.jar filodb.standalone.FiloServer  &
 
 if [ "$SETUP_DATASET" = "YES" ]; then
     echo "Waiting 20s so server can come up ..."
