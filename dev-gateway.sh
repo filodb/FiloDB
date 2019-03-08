7fbb2c75d778466a745e8d1e7a3021832ac82a08
Evan Chan <velvia@apple.com>
2019-03-07 17:05:34 -0800

    Set Prom reporter port differently for Gateway app

--- dev-gateway.sh
+++ dev-gateway.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 java -Dconfig.file=conf/timeseries-filodb-server.conf  \
+     -Dkamon.prometheus.embedded-server.port=9097  \
      -cp gateway/target/scala-2.11/gateway-* filodb.gateway.GatewayServer conf/timeseries-dev-source.conf &
