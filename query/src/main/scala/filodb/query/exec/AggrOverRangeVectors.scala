d64850420beb7050c9f0ff613a4ff0cdf6d5a7e2
Tanvi Bhavsar <tanvibhavsar@MacBook-Pro.local>
2019-03-15 14:20:52 -0700

    Add to count only when input values is not NaN

--- query/src/main/scala/filodb/query/exec/AggrOverRangeVectors.scala
+++ query/src/main/scala/filodb/query/exec/AggrOverRangeVectors.scala
@@ -387,20 +387,15 @@ object CountRowAggregator extends RowAggregator {
   def zero: CountHolder = new CountHolder()
   def newRowToMapInto: MutableRowReader = new TransientRow()
   def map(rvk: RangeVectorKey, item: RowReader, mapInto: MutableRowReader): RowReader = {
-    if (!item.getDouble(1).isNaN) {
-      mapInto.setLong(0, item.getLong(0))
-      mapInto.setDouble(1, 1d)
-    }
-    else {
-      mapInto.setLong(0, item.getLong(0))
-      mapInto.setDouble(1, 0d)
-    }
+    mapInto.setLong(0, item.getLong(0))
+    mapInto.setDouble(1, if (item.getDouble(1).isNaN) 0d else 1d)
     mapInto
   }
   def reduceAggregate(acc: CountHolder, aggRes: RowReader): CountHolder = {
     if (acc.count.isNaN && aggRes.getDouble(1) > 0) acc.count = 0d;
     acc.timestamp = aggRes.getLong(0)
-    acc.count += aggRes.getDouble(1)
+    if (!aggRes.getDouble(1).isNaN)
+      acc.count += aggRes.getDouble(1)
     acc
   }
   def present(aggRangeVector: RangeVector, limit: Int): Seq[RangeVector] = Seq(aggRangeVector)
