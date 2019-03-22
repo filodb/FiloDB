2072534f3928c46a6d6372e1d4c0e1a6f60d2120
Brian O'Neill <broneill@apple.com>
2019-03-21 17:34:04 -0700

    Ditch stop/start ingestion messages and rely on resync messages only. A bunch of tests are still failing, however.

--- coordinator/src/main/scala/filodb.coordinator/ShardStatus.scala
+++ coordinator/src/main/scala/filodb.coordinator/ShardStatus.scala
@@ -20,32 +20,6 @@ final case class CurrentShardSnapshot(ref: DatasetRef,
 final case class ResyncShardIngestion(version: Long, ref: DatasetRef, map: ShardMapper) extends ShardAction
 
 /**
-  * These commands are sent by the NodeClusterActor to the right nodes upon events or
-  * changes to the cluster. For example a new node joins, StartShardIngestion might be sent.
-  * Should start with a verb, since these are commands.
-  */
-sealed trait ShardCommand extends ShardAction {
-  def ref: DatasetRef
-  def shard: Int
-}
-
-final case class StartShardIngestion(ref: DatasetRef, shard: Int, offset: Option[Long]) extends ShardCommand
-
-final case class StopShardIngestion(ref: DatasetRef, shard: Int) extends ShardCommand
-
-/** Direct result of sending an invalid [[ShardCommand]]. It is acked to the
-  * sender if the shard command's shard or dataset is not valid based on the
-  * projection or shard state. It is located with the shard commands because
-  * this is not a potential result of an Ingestion command and flows through
-  * a node's coordinator, one of its ingesters, the cluster shard actor and
-  * its [[filodb.coordinator.ShardAssignmentStrategy]].
-  *
-  * Use cases: result of invalid state and injecting failure to the memstore
-  * during stream creation in the [[StartShardIngestion]] protocol.
-  */
-final case class InvalidIngestionCommand(ref: DatasetRef, shard: Int)
-
-/**
   * The events are sent by the IngestionActor on a node when the actual ingestion stream
   * starts, errors, or by the NodeClusterActor singleton upon detection of node failure / disconnect
   * via Akka Cluster events.
