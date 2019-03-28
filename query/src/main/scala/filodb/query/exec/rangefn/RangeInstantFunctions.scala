ad413a85b7d41cd93aa0422f1858bd1fccc01c04
sherali42 <43357447+sherali42@users.noreply.github.com>
2019-03-28 11:23:11 -0700

    bug(query): resets function bug fix (#300)
    bug(query): update resets function spec to capture empty windows and for data with no counter resets

--- query/src/main/scala/filodb/query/exec/rangefn/RangeInstantFunctions.scala
+++ query/src/main/scala/filodb/query/exec/rangefn/RangeInstantFunctions.scala
@@ -123,8 +123,8 @@ class ResetsFunction extends RangeFunction {
 
   def addedToWindow(row: TransientRow, window: Window): Unit = {
     val size = window.size
+    if (resets.isNaN && size > 0) resets = 0
     if (size > 1 && window(size - 2).value > row.value) {
-      if (resets.isNaN) resets = 0
       resets += 1
     }
   }
