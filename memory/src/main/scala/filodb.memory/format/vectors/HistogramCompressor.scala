ea2e36316b3c93a758288a29d7b06eab46ca9a8d
Evan Chan <velvia@apple.com>
2019-04-15 22:48:17 -0700

    Add PromCompressor to estimate Prometheus-style compression stats

--- memory/src/main/scala/filodb.memory/format/vectors/HistogramCompressor.scala
+++ memory/src/main/scala/filodb.memory/format/vectors/HistogramCompressor.scala
@@ -3,45 +3,71 @@ package filodb.memory.format.vectors
 import scala.io.Source
 
 import org.agrona.ExpandableArrayBuffer
+import scalaxy.loops._
 
 import filodb.memory.NativeMemoryManager
 import filodb.memory.format._
 
+trait CompressorAnalyzer {
+  def inputFile: String
+  def numSamples: Int
+
+  val bucketDef = HistogramBuckets.binaryBuckets64
+
+  // TODO: switch to PageAlignedMemManager so the memory can be constantly recycled
+  val memFactory = new NativeMemoryManager(500 * 1024 * 1024)
+
+  var binHistBytesSum = 0
+  var binHistBytesMax = 0
+  var numRecords = 0
+  var numChunks = 0
+
+  var encodedTotal = 0
+  var writeBufferTotal = 0
+  var samplesEncoded = 0
+
+  lazy val histograms = Source.fromFile(inputFile).getLines
+                         .take(numSamples)
+                         .map { line =>
+                           val buckets = line.split(",").map(_.trim.toLong)
+                           LongHistogram(bucketDef, buckets)
+                         }
+
+  def analyze(): Unit = {
+    // Dump out overall aggregates
+    val avgEncoded = encodedTotal.toDouble / numChunks
+    val avgWriteBuf = writeBufferTotal.toDouble / numChunks
+    val histPerChunk = samplesEncoded.toDouble / numChunks
+    println(s"Total number of chunks: $numChunks")
+    println(s"Samples encoded: $samplesEncoded")
+    println(s"Histograms per chunk: $histPerChunk")
+
+    val normAvgEncoded = avgEncoded * 300 / histPerChunk
+    val normAvgWriteBuf = avgWriteBuf * 300 / histPerChunk
+    println(s"Average encoded chunk size: $avgEncoded (normalized to 300 histograms: $normAvgEncoded)")
+    println(s"Average write buffer size:  $avgWriteBuf (normalized to 300 histograms: $normAvgWriteBuf)")
+    println(s"Compression ratio: ${avgWriteBuf / avgEncoded}")
+    println(s"Average binHistogram size: ${binHistBytesSum / numRecords.toDouble}")
+    println(s"Max binHistogram size: $binHistBytesMax bytes")
+  }
+}
+
 //scalastyle:off
 // Input is a file with one line per histogram, bucket values are comma separated
 // This app is designed to measure histogram compression ratios based on real world histogram data
-object HistogramCompressor extends App {
+object HistogramCompressor extends App with CompressorAnalyzer {
   if (args.length < 1) {
     println("Usage: sbt memory/run <histogram-file> <numsamples>")
     println("Tests chunk compression, not runtime or performance, against real-world increasing bucket histogram files")
     sys.exit(0)
   }
   val inputFile = args(0)
-  var numChunks = 0
   val numSamples = if (args.length > 1) args(1).toInt else 6000
 
-  // TODO: switch to PageAlignedMemManager so the memory can be constantly recycled
-  val memFactory = new NativeMemoryManager(500 * 1024 * 1024)
   val inputBuffer = new ExpandableArrayBuffer(4096)
 
   val maxBytes = 60 * 300   // Maximum allowable histogram writebuffer size
   val appender = HistogramVector.appending(memFactory, maxBytes)
-  val bucketDef = HistogramBuckets.binaryBuckets64
-
-  var binHistBytesSum = 0
-  var binHistBytesMax = 0
-  var numRecords = 0
-
-  var encodedTotal = 0
-  var writeBufferTotal = 0
-  var samplesEncoded = 0
-
-  val histograms = Source.fromFile(inputFile).getLines
-                         .take(numSamples)
-                         .map { line =>
-                           val buckets = line.split(",").map(_.trim.toLong)
-                           LongHistogram(bucketDef, buckets)
-                         }
 
   histograms.foreach { h =>
     val buf = h.serialize()
@@ -72,21 +98,47 @@ object HistogramCompressor extends App {
     }
   }
 
-  // Encode final chunk?  Or just forget it, because it will mess up size statistics?
+  analyze()
+}
 
-  // Dump out overall aggregates
-  val avgEncoded = encodedTotal.toDouble / numChunks
-  val avgWriteBuf = writeBufferTotal.toDouble / numChunks
-  val histPerChunk = samplesEncoded.toDouble / numChunks
-  println(s"Total number of chunks: $numChunks")
-  println(s"Samples encoded: $samplesEncoded")
-  println(s"Histograms per chunk: $histPerChunk")
+// Like HistogramCompressor but uses individual Long writebuffers like Prometheus (time series per bucket)
+object PromCompressor extends App with CompressorAnalyzer {
+  if (args.length < 1) {
+    println("Usage: sbt memory/run <histogram-file> <numsamples>")
+    println("Tests chunk compression, not runtime or performance, against real-world increasing bucket histogram files")
+    sys.exit(0)
+  }
+  val inputFile = args(0)
+  val numSamples = if (args.length > 1) args(1).toInt else 6000
 
-  val normAvgEncoded = avgEncoded * 300 / histPerChunk
-  val normAvgWriteBuf = avgWriteBuf * 300 / histPerChunk
-  println(s"Average encoded chunk size: $avgEncoded (normalized to 300 histograms: $normAvgEncoded)")
-  println(s"Average write buffer size:  $avgWriteBuf (normalized to 300 histograms: $normAvgWriteBuf)")
-  println(s"Compression ratio: ${avgWriteBuf / avgEncoded}")
-  println(s"Average binHistogram size: ${binHistBytesSum / numRecords.toDouble}")
-  println(s"Max binHistogram size: $binHistBytesMax bytes")
+  val appenders = (0 until bucketDef.numBuckets).map { _ =>
+    LongBinaryVector.appendingVectorNoNA(memFactory, 300)
+  }.toArray
+
+  histograms.foreach { h =>
+    numRecords += 1
+    for { b <- 0 until bucketDef.numBuckets optimized } {
+      appenders(b).addData(h.values(b)) match {
+        case Ack =>    // data added, no problem
+        case VectorTooSmall(_, _) =>   // not enough space.   Encode, aggregate, and add to a new appender
+          // Optimize and get optimized size, dump out, aggregate
+          val writeBufSize = appenders.map(_.numBytes).sum
+          val optimized = appenders.map(_.optimize(memFactory))
+          val encodedSize = optimized.map(BinaryVector.totalBytes).sum
+          println(s" WriteBuffer size: ${writeBufSize}\t\tEncoded size: $encodedSize")
+          encodedTotal += encodedSize
+          writeBufferTotal += writeBufSize
+          numChunks += 1
+          samplesEncoded += 300
+
+          appenders.foreach(_.reset())
+          // Add back the input that did not fit into appender again
+          appenders(b).addData(h.values(b))
+        case other =>
+          println(s"Warning: response $other from appender.addData")
+      }
+    }
+  }
+
+  analyze()
 }
