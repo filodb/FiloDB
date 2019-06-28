cf3f552e321c2ff9c11d5f710e0a2cb70b25518e
Evan Chan <velvia@apple.com>
2019-06-28 14:32:26 -0700

    Fix style issue

--- jmh/src/main/scala/filodb.jmh/HistogramIngestBenchmark.scala
+++ jmh/src/main/scala/filodb.jmh/HistogramIngestBenchmark.scala
@@ -1,13 +1,13 @@
 package filodb.jmh
 
-import scala.language.postfixOps
-
 import java.util.concurrent.TimeUnit
+
+import scala.language.postfixOps
 
 import ch.qos.logback.classic.{Level, Logger}
 import com.typesafe.config.ConfigFactory
-import org.agrona.concurrent.UnsafeBuffer
 import org.agrona.{ExpandableArrayBuffer, ExpandableDirectByteBuffer}
+import org.agrona.concurrent.UnsafeBuffer
 import org.openjdk.jmh.annotations.{Level => JMHLevel, _}
 import scalaxy.loops._
 
