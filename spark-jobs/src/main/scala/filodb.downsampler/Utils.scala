2f8fd199d8d5cd07f270576c8d6c9bed1b3b7a40
Vish Ramachandran <vish.ramachandran@apple.com>
2019-09-24 14:11:08 -0700

    time format

--- spark-jobs/src/main/scala/filodb.downsampler/Utils.scala
+++ spark-jobs/src/main/scala/filodb.downsampler/Utils.scala
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat
 import java.util.TimeZone
 
 object Utils {
-  private val sdf = new SimpleDateFormat
+  private val sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
   sdf.setTimeZone(TimeZone.getTimeZone("UTC"))
   def millisToString(millis: Long): String = sdf.format(millis)
 
