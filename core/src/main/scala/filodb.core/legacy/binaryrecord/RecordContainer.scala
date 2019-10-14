81c5afa24b651eaea52d4538525339a79dcaf81b
jackson-paul <jackson_paul@apple.com>
2019-10-14 16:24:11 -0700

    misc(core,gateway):Add old binaryRecord for backward compatability

---
+++ core/src/main/scala/filodb.core/legacy/binaryrecord/RecordContainer.scala
@@ -0,0 +1,161 @@
+package filodb.core.legacy.binaryrecord
+
+import java.nio.ByteBuffer
+
+import scala.reflect.ClassTag
+
+import debox.Buffer
+
+import filodb.memory.{BinaryRegionConsumer, BinaryRegionLarge}
+import filodb.memory.format.{RowReader, UnsafeUtils}
+
+/**
+ * A RecordContainer is a binary, wire-compatible container for BinaryRecords V2.
+ * Example uses: for holding/batching multiple BinaryRecords for Kafka, or as a set of partition keys.
+ * It is also the container to which the RecordBuilder writes into.
+ *
+ * A RecordContainer is compatible with BinaryRegionLarge.
+ *
+ * +0000 4 bytes  length header - number of bytes following this
+ *
+ * @param base null for offheap, or an Array[Byte] for onheap
+ * @param offset 64-bit native pointer or offset into Array[Byte] object memory
+ * @param maxLength the maximum allocated size the container can grow to, including the initial length bytes
+ */
+@Deprecated
+final class RecordContainer(val base: Any, val offset: Long, maxLength: Int,
+                            // INTERNAL variable placed here to avoid Scalac using a bitfield and slowing things down
+                            var numRecords: Int = 0) {
+  import RecordBuilder._
+
+  @inline final def numBytes: Int = UnsafeUtils.getInt(base, offset)
+  @inline final def isEmpty: Boolean = numBytes <= 4
+
+  /**
+   * Used only by the RecordBuilder to update the length field.
+   */
+  private[binaryrecord] def updateLengthWithOffset(endOffset: Long): Unit = {
+    val newLength = endOffset - offset - 4
+    require(newLength >= 0 && newLength <= (maxLength - 4))
+    UnsafeUtils.setInt(base, offset, newLength.toInt)
+  }
+
+  private[binaryrecord] def writeVersionWord(): Unit = {
+    val word = Version << 24
+    UnsafeUtils.setInt(base, offset + 4, word)
+  }
+
+  /**
+   * Iterates through each BinaryRecord location, passing it to the Consumer so that no object allocations
+   * are done.  Method returns when we have iterated through all the records.
+   */
+  final def consumeRecords(consumer: BinaryRegionConsumer): Unit = {
+    val endOffset = offset + 4 + numBytes
+    var curOffset = offset + ContainerHeaderLen
+    while (curOffset < endOffset) {
+      val recordLen = BinaryRegionLarge.numBytes(base, curOffset)
+      consumer.onNext(base, curOffset)
+      curOffset += (recordLen + 7) & ~3   // +4, then aligned/rounded up to next 4 bytes
+    }
+  }
+
+  /**
+   * Iterates through each BinaryRecord as a RowReader.  Results in two allocations: the Iterator
+   * as well as a BinaryRecordRowReader.
+   */
+  final def iterate(schema: RecordSchema): Iterator[RowReader] = new Iterator[RowReader] {
+    val reader = new BinaryRecordRowReader(schema, base)
+    val endOffset = offset + 4 + numBytes
+    var curOffset = offset + ContainerHeaderLen
+
+    final def hasNext: Boolean = curOffset < endOffset
+    final def next: RowReader = {
+      val recordLen = BinaryRegionLarge.numBytes(base, curOffset)
+      reader.recordOffset = curOffset
+      curOffset += (recordLen + 7) & ~3   // +4, then aligned/rounded up to next 4 bytes
+      reader
+    }
+  }
+
+  class FunctionalConsumer(func: (Any, Long) => Unit) extends BinaryRegionConsumer {
+    def onNext(base: Any, offset: Long): Unit = func(base, offset)
+  }
+
+  /**
+   * A convenience method to iterate through each BinaryRecord using a function.  Probably not as efficient
+   * as directly calling consumeRecords, but easier to write.
+   * TODO: make this a MACRO and rewrite it as a custom BinaryRegionConsumer.
+   */
+  def foreach(func: (Any, Long) => Unit): Unit = consumeRecords(new FunctionalConsumer(func))
+
+  /**
+   * Convenience functional method to map the BinaryRecords to something else and returns a Buffer which
+   * can be unboxed and efficient.  If you want performance, consider using consumeRecords instead.
+   */
+  def map[@specialized B: ClassTag](func: (Any, Long) => B): Buffer[B] = {
+    val result = Buffer.empty[B]
+    foreach { case (b, o) => result += func(b, o) }
+    result
+  }
+
+  /**
+   * A convenient method to return all of the record offsets (or native pointers if offheap) for this container.
+   * @return a debox.Buffer[Long], which conveniently does not box, and minimizes memory use.
+   */
+  final def allOffsets: Buffer[Long] = map { case (b, o) => o }
+
+  class CountingConsumer(var count: Int = 0) extends BinaryRegionConsumer {
+    final def onNext(base: Any, offset: Long): Unit = count += 1
+  }
+
+  /**
+   * Uses consumeRecords with a consumer that counts the # of records
+   */
+  final def countRecords(): Int = {
+    val counter = new CountingConsumer()
+    consumeRecords(counter)
+    counter.count
+  }
+
+  /**
+   * Returns true if this RecordContainer is backed by a byte array object on heap.
+   */
+  def hasArray: Boolean = base match {
+    case UnsafeUtils.ZeroPointer => false
+    case b: Array[Byte]          => true
+    case other: Any              => false
+  }
+
+  /**
+   * IF this is an on-heap container, return the Array[Byte] object AS IS (meaning might not be full)
+   * Otherwise throw an Exception
+   */
+  def array: Array[Byte] =
+    if (hasArray) base.asInstanceOf[Array[Byte]] else throw new UnsupportedOperationException("Not an array container")
+
+  /**
+   * Returns a NEW byte array which is of minimal size by copying only the filled bytes.
+   * Can also be used to copy offheap containers to onheap byte array.
+   */
+  def trimmedArray: Array[Byte] = {
+    val newBytes = new Array[Byte](numBytes + 4)
+    UnsafeUtils.unsafe.copyMemory(base, offset, newBytes, UnsafeUtils.arayOffset, numBytes + 4)
+    newBytes
+  }
+}
+
+@Deprecated
+object RecordContainer {
+  /**
+   * Returns a read-only container; updateLength will never work.
+   */
+  def apply(array: Array[Byte]): RecordContainer = new RecordContainer(array, UnsafeUtils.arayOffset, 0)
+
+  /**
+   * Returns a read-only container given a ByteBuffer wrapper
+   */
+  def apply(buf: ByteBuffer): RecordContainer = {
+    val (base, offset, _) = UnsafeUtils.BOLfromBuffer(buf)
+    new RecordContainer(base, offset, 0)
+  }
+}
---
+++ gateway/src/main/scala/filodb/gateway/legacy/conversion/InputRecord.scala
@@ -0,0 +1,162 @@
+package filodb.gateway.legacy.conversion
+
+import scala.language.postfixOps
+
+import remote.RemoteStorage.TimeSeries
+import scalaxy.loops._
+
+import filodb.core.legacy.binaryrecord.RecordBuilder
+import filodb.core.metadata.Dataset
+import filodb.memory.format.{SeqRowReader, ZeroCopyUTF8String => ZCUTF8}
+
+/**
+ * An InputRecord represents one "record" of timeseries data for input to FiloDB system.
+ * It knows how to extract the information necessary for shard calculation as well as
+ * how to add the right fields to the RecordBuilder.
+ */
+@Deprecated
+trait InputRecord {
+  /**
+   * The shardKeyHash and partitionKeyHash for ShardMappers.ingestionShard method
+   */
+  def shardKeyHash: Int
+  def partitionKeyHash: Int
+
+  /**
+   * The values for each of the tag keys found in DatasetOptions.nonMetricShardColumns
+   * @return the nonMetricShardColumns tag values, in the same order as nonMetricShardColumns
+   *         If any tag/key is not found, then the Seq will be truncated at the last found value.
+   */
+  def nonMetricShardValues: Seq[String]
+
+  // This is the metric value needed for shard spread calculation
+  def getMetric: String
+
+  /**
+   * Adds the contents of this record to RecordBuilder as one or more Filo records
+   */
+  def addToBuilder(builder: RecordBuilder): Unit
+}
+
+/**
+ * A Prometheus-format time series input record.
+ * Logic in here does the following conversions:
+ * - Shard/partition hashes are calculated scuh that histogram time series go to the same shard.
+ *   IE _bucket _sum _count are stripped off of all metric names.
+ *   Similarly "le" is stripped off of Prom tags for shard calculation purposes
+ * shardKeys in tags are used to compute the shardKeyHash, all other tags are used to compute
+ * the partition key hash.
+ * The tags should NOT include the metric name.
+ */
+@Deprecated
+case class PrometheusInputRecord(tags: Map[String, String],
+                                 metric: String,
+                                 dataset: Dataset,
+                                 timestamp: Long,
+                                 value: Double) extends InputRecord {
+  import collection.JavaConverters._
+
+  val trimmedMetric = RecordBuilder.trimShardColumn(dataset, dataset.options.metricColumn, metric)
+  val javaTags = new java.util.ArrayList(tags.toSeq.asJava)
+
+  // Get hashes and sort tags of the keys/values for shard calculation
+  val hashes = RecordBuilder.sortAndComputeHashes(javaTags)
+
+  final def shardKeyHash: Int = RecordBuilder.shardKeyHash(nonMetricShardValues, trimmedMetric)
+  final def partitionKeyHash: Int = RecordBuilder.combineHashExcluding(javaTags, hashes,
+                                                    dataset.options.ignorePartKeyHashTags)
+
+  val nonMetricShardValues: Seq[String] = dataset.options.nonMetricShardColumns.flatMap(tags.get)
+  final def getMetric: String = metric
+
+  final def addToBuilder(builder: RecordBuilder): Unit = {
+    builder.startNewRecord()
+    builder.addLong(timestamp)
+    builder.addDouble(value)
+    builder.startMap()
+    val metricBytes = metric.getBytes
+    builder.addMapKeyValueHash(dataset.options.metricBytes, dataset.options.metricHash,
+                               metricBytes, 0, metricBytes.size)
+    for { i <- 0 until javaTags.size optimized } {
+      val (k, v) = javaTags.get(i)
+      builder.addMapKeyValue(k.getBytes, v.getBytes)
+      builder.updatePartitionHash(hashes(i))
+    }
+    builder.endMap(bulkHash = false)
+
+    builder.endRecord()
+  }
+}
+
+@Deprecated
+object PrometheusInputRecord {
+  val DefaultShardHash = -1
+
+  // Create PrometheusInputRecords from a TimeSeries protobuf object
+  def apply(tsProto: TimeSeries, dataset: Dataset): Seq[PrometheusInputRecord] = {
+    val tags = (0 until tsProto.getLabelsCount).map { i =>
+      val labelPair = tsProto.getLabels(i)
+      (labelPair.getName, labelPair.getValue)
+    }
+    val metricTags = tags.filter(_._1 == dataset.options.metricColumn)
+    if (metricTags.isEmpty) {
+      Nil
+    } else {
+      val metric = metricTags.head._2
+      val transformedTags = transformTags(tags.filterNot(_._1 == dataset.options.metricColumn), dataset)
+      (0 until tsProto.getSamplesCount).map { i =>
+        val sample = tsProto.getSamples(i)
+        PrometheusInputRecord(transformedTags, metric, dataset, sample.getTimestampMs, sample.getValue)
+      }
+    }
+  }
+
+  /**
+   * Uses DatasetOptions.copyTags to copy missing tags.
+   * If a tag in copyTags is found and the destination tag is missing, then the destination tag is created
+   * with the value from the source tag.
+   */
+  def transformTags(tags: Seq[(String, String)], dataset: Dataset): Map[String, String] = {
+    val extraTags = new collection.mutable.HashMap[String, String]()
+    val tagsMap = tags.toMap
+    for ((k, v) <- dataset.options.copyTags) {
+      if (!extraTags.contains(v)
+        && !tagsMap.contains(v)
+        && tagsMap.contains(k)) {
+        extraTags += v -> tagsMap(k)
+      }
+    }
+    tagsMap ++ extraTags
+  }
+}
+
+/**
+ * A generic InputRecord that can serve different data schemas, so long as the partition key consists of:
+ *  - a single metric StringColumn
+ *  - a MapColumn of tags
+ *
+ * Can be used to adapt custom dataset/schemas for input into FiloDB using the gateway.
+ * Not going to be the fastest InputRecord but extremely flexible.
+ *
+ * @param values the data column values, first one is probably timestamp
+ */
+@Deprecated
+class MetricTagInputRecord(values: Seq[Any],
+                           metric: String,
+                           tags: Map[ZCUTF8, ZCUTF8],
+                           dataset: Dataset) extends InputRecord {
+  final def shardKeyHash: Int = RecordBuilder.shardKeyHash(nonMetricShardValues, metric)
+  // NOTE: this is probably not very performant right now.
+  final def partitionKeyHash: Int = tags.hashCode
+
+  val nonMetricShardValues: Seq[String] =
+    dataset.options.nonMetricShardKeyUTF8.flatMap(tags.get).map(_.toString).toSeq
+  final def getMetric: String = metric
+
+  def addToBuilder(builder: RecordBuilder): Unit = {
+    require(builder.schema == dataset.ingestionSchema)
+
+    val reader = SeqRowReader(values :+ metric :+ tags)
+    builder.addFromReader(reader)
+  }
+}
