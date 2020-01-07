513982f1ea2dbdf287aa3f74629c5e11653e03a6
Tanvi Bhavsar <tanvibhavsar@MacBook-Pro-4.local>
2020-01-06 21:30:50 -0800

    fixed indentation

--- prometheus/src/main/scala/filodb/prometheus/ast/Functions.scala
+++ prometheus/src/main/scala/filodb/prometheus/ast/Functions.scala
@@ -108,16 +108,16 @@ trait Functions extends Base with Operators with Vectors {
         val periodicSeriesPlan = seriesParam.asInstanceOf[PeriodicSeries].toPeriodicSeriesPlan(timeParams)
         ApplyAbsentFunction(periodicSeriesPlan, columnFilter, RangeParams(timeParams.start, timeParams.step,
           timeParams.end))
-        } else {
-          val rangeFunctionId = RangeFunctionId.withNameInsensitiveOption(name).get
-          val rangeExpression = seriesParam.asInstanceOf[RangeExpression]
-          PeriodicSeriesWithWindowing(
-            rangeExpression.toRawSeriesPlan(timeParams, isRoot = false).asInstanceOf[RawSeries],
-            timeParams.start * 1000, timeParams.step * 1000, timeParams.end * 1000,
-            rangeExpression.window.millis,
-            rangeFunctionId, otherParams)
-        }
+      } else {
+        val rangeFunctionId = RangeFunctionId.withNameInsensitiveOption(name).get
+        val rangeExpression = seriesParam.asInstanceOf[RangeExpression]
+        PeriodicSeriesWithWindowing(
+          rangeExpression.toRawSeriesPlan(timeParams, isRoot = false).asInstanceOf[RawSeries],
+          timeParams.start * 1000, timeParams.step * 1000, timeParams.end * 1000,
+          rangeExpression.window.millis,
+          rangeFunctionId, otherParams)
       }
     }
+  }
 }
 
