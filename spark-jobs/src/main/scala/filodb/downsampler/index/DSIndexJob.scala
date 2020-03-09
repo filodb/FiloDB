9edcc41a22d6cf5d809de3a5ffc6dc69ab7e74e0
sherali42 <43357447+sherali42@users.noreply.github.com>
2020-03-09 15:57:02 -0700

    misc(spark-jobs): remove kamon instrumentation from downsample index job (#676)

--- spark-jobs/src/main/scala/filodb/downsampler/index/DSIndexJob.scala
+++ spark-jobs/src/main/scala/filodb/downsampler/index/DSIndexJob.scala
@@ -2,7 +2,6 @@ package filodb.downsampler.index
 
 import scala.concurrent.Await
 
-import kamon.Kamon
 import monix.reactive.Observable
 
 import filodb.cassandra.columnstore.CassandraColumnStore
@@ -17,11 +16,11 @@ import filodb.memory.format.UnsafeUtils
 class DSIndexJob(dsSettings: DownsamplerSettings,
                  dsJobsettings: DSIndexJobSettings) extends Instance with Serializable {
 
-  @transient lazy private val sparkTasksStarted = Kamon.counter("spark-tasks-started").withoutTags()
+  /*@transient lazy private val sparkTasksStarted = Kamon.counter("spark-tasks-started").withoutTags()
   @transient lazy private val sparkForeachTasksCompleted = Kamon.counter("spark-foreach-tasks-completed")
                                                                .withoutTags()
   @transient lazy private val sparkTasksFailed = Kamon.counter("spark-tasks-failed").withoutTags()
-  @transient lazy private val totalPartkeysUpdated = Kamon.counter("total-partkeys-updated").withoutTags()
+  @transient lazy private val totalPartkeysUpdated = Kamon.counter("total-partkeys-updated").withoutTags()*/
 
   @transient lazy private[downsampler] val schemas = Schemas.fromConfig(dsSettings.filodbConfig).get
 
@@ -54,11 +53,11 @@ class DSIndexJob(dsSettings: DownsamplerSettings,
 
   def updateDSPartKeyIndex(shard: Int, fromHour: Long, toHour: Long): Unit = {
 
-    sparkTasksStarted.increment
+    /*sparkTasksStarted.increment
     val span = Kamon.spanBuilder("per-shard-index-migration-latency")
       .asChildOf(Kamon.currentSpan())
       .tag("shard", shard)
-      .start
+      .start*/
     val rawDataSource = rawCassandraColStore
 
     @volatile var count = 0
@@ -78,15 +77,15 @@ class DSIndexJob(dsSettings: DownsamplerSettings,
         DownsamplerContext.dsLogger.info(s"Partial Partkey index migration successful for shard=$shard count=$count" +
           s" from=$fromHour to=$toHour")
       }
-      sparkForeachTasksCompleted.increment()
-      totalPartkeysUpdated.increment(count)
+      //sparkForeachTasksCompleted.increment()
+      //totalPartkeysUpdated.increment(count)
     } catch { case e: Exception =>
       DownsamplerContext.dsLogger.error(s"Exception in task count=$count " +
         s"shard=$shard from=$fromHour to=$toHour", e)
-      sparkTasksFailed.increment
+      //sparkTasksFailed.increment
       throw e
     } finally {
-      span.finish()
+//      span.finish()
 //      rawCassandraColStore.shutdown()
 //      downsampleCassandraColStore.shutdown()
     }
