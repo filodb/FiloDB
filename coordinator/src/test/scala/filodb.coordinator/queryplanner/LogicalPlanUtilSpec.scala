4898b7bd2e0689a507c9b5b90ce1983cbae68f62
jackson-paul <jackson_paul@apple.com>
2020-05-19 17:14:49 -0700

    Add operator support

--- coordinator/src/test/scala/filodb.coordinator/queryplanner/LogicalPlanUtilSpec.scala
+++ coordinator/src/test/scala/filodb.coordinator/queryplanner/LogicalPlanUtilSpec.scala
@@ -1,7 +1,7 @@
 package filodb.coordinator.queryplanner
 
 import filodb.core.query.ColumnFilter
-import filodb.core.query.Filter.{Equals, In}
+import filodb.core.query.Filter.{Equals, EqualsRegex, In, NotEquals, NotEqualsRegex}
 import filodb.query.BinaryOperator.DIV
 import filodb.query.Cardinality.OneToOne
 import filodb.query.RangeFunctionId.SumOverTime
@@ -13,13 +13,18 @@ class LogicalPlanUtilsSpec extends FunSpec with Matchers {
   it("should get labelValueOps from logicalPlan") {
 
     val rawSeries = RawSeries(IntervalSelector(1000, 3000), Seq(ColumnFilter("_name_", Equals("MetricName")),
-      ColumnFilter("instance", Equals("Inst-0"))), Seq("_name_", "instance"), Some(300000), None)
+      ColumnFilter("instance", NotEquals("Inst-0"))), Seq("_name_", "instance"), Some(300000), None)
     val periodicSeriesWithWindowing = PeriodicSeriesWithWindowing(rawSeries, 1000, 500, 5000, 100, SumOverTime)
 
     val res = LogicalPlanUtils.getLabelValueOperatorsFromLogicalPlan(periodicSeriesWithWindowing)
-    println(res)
-//    Some(List(LabelValueOperatorGroup(List(LabelValueOperator(_name_,Set(MetricName),<function1>), LabelValueOperator(instance,Set(Inst-0),<function1>)))))
-//    res.get.shouldEqual(Seq("MetricName"))
+    res.get.size.shouldEqual(1)
+    res.get(0).labelValueOperators.size.shouldEqual(2)
+    res.get(0).labelValueOperators(0).columnName.shouldEqual("_name_")
+    res.get(0).labelValueOperators(0).value.shouldEqual(Set("MetricName"))
+    res.get(0).labelValueOperators(0).operator.shouldEqual("=")
+    res.get(0).labelValueOperators(1).columnName.shouldEqual("instance")
+    res.get(0).labelValueOperators(1).value.shouldEqual(Set("Inst-0"))
+    res.get(0).labelValueOperators(1).operator.shouldEqual("!=")
   }
 
   it("should get labelValueOps from logicalPlan with filter In") {
@@ -29,28 +34,44 @@ class LogicalPlanUtilsSpec extends FunSpec with Matchers {
     val periodicSeriesWithWindowing = PeriodicSeriesWithWindowing(rawSeries, 1000, 500, 5000, 100, SumOverTime)
 
     val res = LogicalPlanUtils.getLabelValueOperatorsFromLogicalPlan(periodicSeriesWithWindowing)
-    //Some(List(LabelValueOperatorGroup(List(LabelValueOperator(_name_,Set(MetricName),<function1>), LabelValueOperator(instance,Set(Inst-0, Inst-1),<function1>)))))
-    println(res)
-//    res.get.shouldEqual(Seq("Inst-0", "Inst-1"))
+    res.get.size.shouldEqual(1)
+    res.get(0).labelValueOperators.size.shouldEqual(2)
+    res.get(0).labelValueOperators(0).columnName.shouldEqual("_name_")
+    res.get(0).labelValueOperators(0).value.shouldEqual(Set("MetricName"))
+    res.get(0).labelValueOperators(0).operator.shouldEqual("=")
+    res.get(0).labelValueOperators(1).columnName.shouldEqual("instance")
+    res.get(0).labelValueOperators(1).value.shouldEqual(Set("Inst-0", "Inst-1"))
+    res.get(0).labelValueOperators(1).operator.shouldEqual("in")
   }
 
   it("should get labelValueOps from BinaryJoin LogicalPlan") {
 
     val rawSeriesLhs = RawSeries(IntervalSelector(1000, 3000), Seq(ColumnFilter("_name_", Equals("MetricName1")),
-      ColumnFilter("instance", Equals("Inst-0"))), Seq("_name_", "instance"), Some(300000), None)
+      ColumnFilter("instance", EqualsRegex("Inst-0"))), Seq("_name_", "instance"), Some(300000), None)
     val lhs = PeriodicSeries(rawSeriesLhs, 1000, 500, 50000)
 
     val rawSeriesRhs = RawSeries(IntervalSelector(1000, 3000), Seq(ColumnFilter("job", Equals("MetricName2")),
-      ColumnFilter("instance", Equals("Inst-1"))), Seq("job", "instance"), Some(300000), None)
+      ColumnFilter("instance", NotEqualsRegex("Inst-1"))), Seq("job", "instance"), Some(300000), None)
     val rhs = PeriodicSeries(rawSeriesRhs, 1000, 500, 50000)
 
     val binaryJoin = BinaryJoin(lhs, DIV, OneToOne, rhs)
 
     val res = LogicalPlanUtils.getLabelValueOperatorsFromLogicalPlan(binaryJoin)
-//    Some(List(LabelValueOperatorGroup(List(LabelValueOperator(_name_,Set(MetricName1),<function1>), LabelValueOperator(instance,Set(Inst-0),<function1>))),
-    //    LabelValueOperatorGroup(List(LabelValueOperator(job,Set(MetricName2),<function1>), LabelValueOperator(instance,Set(Inst-1),<function1>)))))
-    println(res)
-    //    res.get.shouldEqual(Seq("MetricName1"))
+    res.get.size.shouldEqual(2)
+    res.get(0).labelValueOperators.size.shouldEqual(2)
+    res.get(0).labelValueOperators(0).columnName.shouldEqual("_name_")
+    res.get(0).labelValueOperators(0).value.shouldEqual(Set("MetricName1"))
+    res.get(0).labelValueOperators(0).operator.shouldEqual("=")
+    res.get(0).labelValueOperators(1).columnName.shouldEqual("instance")
+    res.get(0).labelValueOperators(1).value.shouldEqual(Set("Inst-0"))
+    res.get(0).labelValueOperators(1).operator.shouldEqual("=~")
+    res.get(1).labelValueOperators.size.shouldEqual(2)
+    res.get(1).labelValueOperators(0).columnName.shouldEqual("job")
+    res.get(1).labelValueOperators(0).value.shouldEqual(Set("MetricName2"))
+    res.get(1).labelValueOperators(0).operator.shouldEqual("=")
+    res.get(1).labelValueOperators(1).columnName.shouldEqual("instance")
+    res.get(1).labelValueOperators(1).value.shouldEqual(Set("Inst-1"))
+    res.get(1).labelValueOperators(1).operator.shouldEqual("!~")
   }
 
   it("should get MetricName from logicalPlan") {
