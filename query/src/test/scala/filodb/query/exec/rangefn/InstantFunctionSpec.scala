42c31e8b11cfc79eedb39c05ec20057411ba1ac7
Brian O'Neill <broneill@apple.com>
2020-06-30 14:28:19 -0700

    Use explicit close.

--- query/src/test/scala/filodb/query/exec/rangefn/InstantFunctionSpec.scala
+++ query/src/test/scala/filodb/query/exec/rangefn/InstantFunctionSpec.scala
@@ -326,20 +326,19 @@ class InstantFunctionSpec extends RawDataWindowingSpec with ScalaFutures {
                                 schema: ResultSchema = resultSchema): Unit = {
     val instantVectorFnMapper = exec.InstantVectorFunctionMapper(instantFunctionId, funcParams.map(x => StaticFuncArgs(x, RangeParams(100,10,200))))
     val resultObs = instantVectorFnMapper(Observable.fromIterable(samples), querySession, 1000, schema, Nil)
-    val result = resultObs.toListL.runAsync.futureValue.map(_.rows.map(_.getDouble(1)))
+    val result = resultObs.toListL.runAsync.futureValue.map(_.rows)
     expectedVal.zip(result).foreach {
       case (ex, res) =>  {
         ex.zip(res).foreach {
-          case (val1, val2) =>
-            if (val1.isInfinity) val2.isInfinity shouldEqual true
-            else if (val1.isNaN) val2.isNaN shouldEqual true
-            else val1 shouldEqual val2 +- 0.0001
+          case (val1, val2) => {
+            val val2Num = val2.getDouble(1)
+            if (val1.isInfinity) val2Num.isInfinity shouldEqual true
+            else if (val1.isNaN) val2Num.isNaN shouldEqual true
+            else val1 shouldEqual val2Num +- 0.0001
+          }
         }
-        // The zip method appears to short circuit and doesn't always fully drain all the
-        // iterators. This can leave locks stuck. Calling size has the side effect of fully
-        // draining the iterator, which will in turn ensure that locks are released.
-        ex.size
-        res.size
+        // Ensure that locks are released from H2DoubleInstantFuncIterator.
+        res.close();
       }
     }
   }
