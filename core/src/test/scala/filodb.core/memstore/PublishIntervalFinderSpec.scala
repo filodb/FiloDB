f11a15bff18ed3479c937cab32eae963e2ddc6fd
Vish Ramachandran <vish.ramachandran@apple.com>
2020-07-16 11:43:58 -0700

    Unit test for pub int finder

---
+++ core/src/test/scala/filodb.core/memstore/PublishIntervalFinderSpec.scala
@@ -0,0 +1,41 @@
+package filodb.core.memstore
+
+import org.scalatest.{BeforeAndAfter, FunSpec, Matchers}
+import org.scalatest.concurrent.ScalaFutures
+
+import filodb.core.binaryrecord2.RecordBuilder
+import filodb.core.metadata.Schemas
+import filodb.memory.NativeMemoryManager
+import filodb.memory.format.{UnsafeUtils, ZeroCopyUTF8String}
+
+class PublishIntervalFinderSpec  extends FunSpec with Matchers with ScalaFutures with BeforeAndAfter {
+
+  val nativeMemoryManager = new NativeMemoryManager(300000, Map.empty)
+
+  after {
+    nativeMemoryManager.shutdown()
+  }
+
+  it ("should extract step tag for publish interval when present") {
+    import ZeroCopyUTF8String._
+    val seriesTags = Map("_ws_".utf8 -> "my_ws".utf8, "_ns_".utf8 -> "my_ns".utf8, "_step_".utf8 -> "10".utf8)
+    val partBuilder = new RecordBuilder(nativeMemoryManager)
+    val partKey = partBuilder.partKeyFromObjects(Schemas.promCounter, "counterName".utf8, seriesTags)
+    val pubInt = StepTagPublishIntervalFinder.findPublishIntervalMs(
+      Schemas.promCounter.partition.hash, UnsafeUtils.ZeroArray,
+      partKey)
+    pubInt shouldEqual Some(10000)
+  }
+
+  it ("should extract return None for step tag for publish interval when absent") {
+    import ZeroCopyUTF8String._
+    val seriesTags = Map("_ws_".utf8 -> "my_ws".utf8, "_ns_".utf8 -> "my_ns".utf8)
+    val partBuilder = new RecordBuilder(nativeMemoryManager)
+    val partKey = partBuilder.partKeyFromObjects(Schemas.promCounter, "counterName".utf8, seriesTags)
+    val pubInt = StepTagPublishIntervalFinder.findPublishIntervalMs(
+      Schemas.promCounter.partition.hash, UnsafeUtils.ZeroArray,
+      partKey)
+    pubInt shouldEqual None
+  }
+
+}
