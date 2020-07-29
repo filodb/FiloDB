5ad858212f6f6928c4a4a6901eda4fb0a9b70b9c
Szymon Matejczyk <szymon@sumologic.com>
2020-07-29 23:04:59 +0200

    misc(coordinator): RewriteDeprecatedNames for scalatest upgrade

--- coordinator/src/multi-jvm/scala/filodb.coordinator/ClusterSpec.scala
+++ coordinator/src/multi-jvm/scala/filodb.coordinator/ClusterSpec.scala
@@ -3,12 +3,11 @@ package filodb.coordinator
 import akka.remote.testkit.{MultiNodeConfig, MultiNodeSpec}
 import akka.testkit.ImplicitSender
 import com.typesafe.scalalogging.StrictLogging
-import org.scalatest.FunSpecLike
-
 import filodb.core.AsyncTest
+import org.scalatest.funspec.AnyFunSpecLike
 
 abstract class ClusterSpec(config: MultiNodeConfig) extends MultiNodeSpec(config)
-  with FunSpecLike with StrictLogging with ImplicitSender with AsyncTest {
+  with AnyFunSpecLike with StrictLogging with ImplicitSender with AsyncTest {
 
   val cluster = FilodbCluster(system)
 }
