49a7cd5603b70313a5a7e689b1db9b7938851304
Brian S. O'Neill <bronee@gmail.com>
2020-08-11 13:13:48 -0700

    Disable annoying style checking.

--- coordinator/src/main/scala/org/sisioh/akka/cluster/custom/downing/strategy/ClusterCustomDowning.scala
+++ coordinator/src/main/scala/org/sisioh/akka/cluster/custom/downing/strategy/ClusterCustomDowning.scala
@@ -14,7 +14,8 @@ trait ClusterCustomDowning extends ActorLogging { base: CustomAutoDownBase =>
   override protected def scheduler: Scheduler = {
     if (context.system.scheduler.maxFrequency < 1.second / cluster.settings.SchedulerTickDuration) {
       log.warning(
-        "CustomDowning does not use a cluster dedicated scheduler. Cluster will use a dedicated scheduler if configured " +
+        "CustomDowning does not use a cluster dedicated scheduler. " +
+         "Cluster will use a dedicated scheduler if configured " +
         "with 'akka.scheduler.tick-duration' [{} ms] >  'akka.cluster.scheduler.tick-duration' [{} ms].",
         (1000 / context.system.scheduler.maxFrequency).toInt,
         cluster.settings.SchedulerTickDuration.toMillis
