51042439e6285a6603246df9862e2bcaeeb13b8a
Szymon Matejczyk <szymon@sumologic.com>
2020-09-12 12:52:49 +0200

    misc(coordinator): move kamon.init() to top

--- coordinator/src/main/scala/filodb.coordinator/FilodbCluster.scala
+++ coordinator/src/main/scala/filodb.coordinator/FilodbCluster.scala
@@ -238,12 +238,14 @@ final class FilodbCluster(val system: ExtendedActorSystem, overrideConfig: Confi
 
 }
 
+private[filodb] trait KamonInit {
+  Kamon.init()
+}
+
 /** Mixin for easy usage of the FiloDBCluster Extension.
   * Used by all `ClusterRole` nodes starting an ActorSystem and FiloDB Cluster nodes.
   */
-private[filodb] trait FilodbClusterNode extends NodeConfiguration with StrictLogging {
-  Kamon.init()
-
+private[filodb] trait FilodbClusterNode extends NodeConfiguration with StrictLogging with KamonInit {
   def role: ClusterRole
 
   /** Override to pass in additional module config. */
