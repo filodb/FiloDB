d9d03de4ec8f00985a8c8a9cc45115d5dee71df5
Vish Ramachandran <vish.ramachandran@apple.com>
2020-10-06 14:56:27 -0700

    Metric for forced shutdown

--- memory/src/main/scala/filodb.memory/data/Shutdown.scala
+++ memory/src/main/scala/filodb.memory/data/Shutdown.scala
@@ -1,10 +1,13 @@
 package filodb.memory.data
 
 import com.typesafe.scalalogging.StrictLogging
+import kamon.Kamon
 
 object Shutdown extends StrictLogging {
 
+  val forcedShutdowns = Kamon.counter("forced-shutdowns").withoutTags()
   def haltAndCatchFire(e: Exception, unitTest: Boolean = false): Unit = {
+    forcedShutdowns.increment()
     if (unitTest) throw e
     logger.error(s"Shutting down process since it may be in an unstable/corrupt state", e)
     Runtime.getRuntime.halt(189)
