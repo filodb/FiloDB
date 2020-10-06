59ee95b110e2f4dccedadd5817602a70cc740d59
Vish Ramachandran <vish.ramachandran@apple.com>
2020-10-06 07:36:56 -0700

    bug(core): Mark used blocks eeclaimable even after flush errors

---
+++ memory/src/main/scala/filodb.memory/data/Shutdown.scala
@@ -0,0 +1,15 @@
+package filodb.memory.data
+
+import com.typesafe.scalalogging.StrictLogging
+
+object Shutdown extends StrictLogging {
+
+  def haltAndCatchFire(e: Exception, unitTest: Boolean = false): Unit = {
+    if (unitTest) {
+      throw new Error(e)
+    }
+    logger.error(s"Shutting down process since it may be in an unstable/corrupt state", e)
+    Runtime.getRuntime.halt(189)
+  }
+
+}
