b811b4d11c36bc5c62cac985d362d1688ce7982b
Vish Ramachandran <vish.ramachandran@apple.com>
2020-10-06 09:15:13 -0700

    PR feedback

--- memory/src/main/scala/filodb.memory/data/Shutdown.scala
+++ memory/src/main/scala/filodb.memory/data/Shutdown.scala
@@ -5,9 +5,7 @@ import com.typesafe.scalalogging.StrictLogging
 object Shutdown extends StrictLogging {
 
   def haltAndCatchFire(e: Exception, unitTest: Boolean = false): Unit = {
-    if (unitTest) {
-      throw new Error(e)
-    }
+    if (unitTest) throw e
     logger.error(s"Shutting down process since it may be in an unstable/corrupt state", e)
     Runtime.getRuntime.halt(189)
   }
