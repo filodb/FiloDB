80fc106dba1b6369bd81839b126314ecf2fd7fd5
Vish Ramachandran <vish.ramachandran@apple.com>
2021-01-06 14:31:02 -0800

    restore run_benchmarks

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -2,4 +2,10 @@
 sbt "jmh/jmh:run -rf json -i 15 -wi 10 -f3 -jvmArgsAppend -XX:MaxInlineLevel=20 \
  -jvmArgsAppend -Xmx4g -jvmArgsAppend -XX:MaxInlineSize=99 \
  -prof jmh.extras.JFR:dir=/tmp/filo-jmh \
- filodb.jmh.QueryHiCardInMemoryBenchmark"
+ filodb.jmh.QueryHiCardInMemoryBenchmark \
+ filodb.jmh.QueryInMemoryBenchmark \
+ filodb.jmh.QueryAndIngestBenchmark \
+ filodb.jmh.IngestionBenchmark \
+ filodb.jmh.QueryOnDemandBenchmark \
+ filodb.jmh.GatewayBenchmark \
+ filodb.jmh.PartKeyIndexBenchmark"
