3499d7b818d0605b33bc21c87cdfcab5052ffadd
Vish Ramachandran <vish.ramachandran@apple.com>
2021-03-30 11:45:45 -0700

    Temporarily remove capability to return partial results on timeout - should go into another PR

--- query/src/main/scala/filodb/query/exec/PlanDispatcher.scala
+++ query/src/main/scala/filodb/query/exec/PlanDispatcher.scala
@@ -5,14 +5,13 @@ import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
 
 import akka.actor.ActorRef
-import akka.pattern.{ask, AskTimeoutException}
+import akka.pattern.ask
 import akka.util.Timeout
 import monix.eval.Task
 import monix.execution.Scheduler
 
 import filodb.core.QueryTimeoutException
-import filodb.core.query.ResultSchema
-import filodb.query.{Query, QueryResponse, QueryResult}
+import filodb.query.QueryResponse
 
 /**
   * This trait externalizes distributed query execution strategy
@@ -40,12 +39,14 @@ case class ActorPlanDispatcher(target: ActorRef) extends PlanDispatcher {
       val fut = (target ? plan)(t).map {
         case resp: QueryResponse => resp
         case e =>  throw new IllegalStateException(s"Received bad response $e")
-      }.recover { // if partial results allowed, then return empty result
-        case e: AskTimeoutException if (plan.queryContext.plannerParams.allowPartialResults) =>
-            Query.qLogger.warn(s"Swallowed AskTimeoutException since partial result was enabled: ${e.getMessage}")
-            QueryResult(plan.queryContext.queryId, ResultSchema.empty, Nil, true,
-              Some("Result may be partial since query on some shards timed out"))
       }
+      // TODO We can send partial results on timeout. Try later. Need to address QueryTimeoutException too.
+//        .recover { // if partial results allowed, then return empty result
+//        case e: AskTimeoutException if (plan.queryContext.plannerParams.allowPartialResults) =>
+//            Query.qLogger.warn(s"Swallowed AskTimeoutException since partial result was enabled: ${e.getMessage}")
+//            QueryResult(plan.queryContext.queryId, ResultSchema.empty, Nil, true,
+//              Some("Result may be partial since query on some shards timed out"))
+//      }
       Task.fromFuture(fut)
     }
   }
