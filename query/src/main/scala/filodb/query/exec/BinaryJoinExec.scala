ee26696e8b48da880cf442882c8e0d4497aeeaf2
Vish Ramachandran <vish.ramachandran@apple.com>
2021-03-30 11:17:02 -0700

    bug(query): More information in Binary Join Errors to help user

--- query/src/main/scala/filodb/query/exec/BinaryJoinExec.scala
+++ query/src/main/scala/filodb/query/exec/BinaryJoinExec.scala
@@ -100,9 +100,8 @@ final case class BinaryJoinExec(queryContext: QueryContext,
           if (rv.key.labelValues == rvDupe.key.labelValues) {
             oneSideMap.put(jk, StitchRvsExec.stitch(rv, rvDupe))
           } else {
-            qLogger.info(s"BinaryJoinError: RV ${rv.key} (IDs ${rv.key.partIds}) produced $jk, but already in map: " +
-              s"RV ${oneSideMap(jk).key} (IDs ${oneSideMap(jk).key.partIds})")
-            throw new BadQueryException(s"Cardinality $cardinality was used, but many found instead of one for $jk")
+            throw new BadQueryException(s"Cardinality $cardinality was used, but many found instead of one for $jk. " +
+              s"${rvDupe.key.labelValues} and ${rv.key.labelValues} were the violating keys on many side")
           }
         } else {
           oneSideMap.put(jk, rv)
@@ -121,8 +120,8 @@ final case class BinaryJoinExec(queryContext: QueryContext,
             if (resVal.stitchedOtherSide.key.labelValues == rvOther.key.labelValues) {
               StitchRvsExec.stitch(rvOther, resVal.stitchedOtherSide)
             } else {
-              throw new BadQueryException(s"Non-unique result vectors found for $resKey. " +
-                s"Use grouping to create unique matching")
+              throw new BadQueryException(s"Non-unique result vectors ${resVal.stitchedOtherSide.key.labelValues} " +
+                s"and ${rvOther.key.labelValues} found for $resKey . Use grouping to create unique matching")
             }
           } else {
             rvOther
