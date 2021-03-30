0e5438411f1a644fac504cc20ba27eaaff1e62a3
Vish Ramachandran <vish.ramachandran@apple.com>
2021-03-30 12:26:15 -0700

    compile issue

--- query/src/main/scala/filodb/query/exec/BinaryJoinExec.scala
+++ query/src/main/scala/filodb/query/exec/BinaryJoinExec.scala
@@ -12,7 +12,6 @@ import filodb.core.query._
 import filodb.memory.format.{RowReader, ZeroCopyUTF8String => Utf8Str}
 import filodb.memory.format.ZeroCopyUTF8String._
 import filodb.query._
-import filodb.query.Query.qLogger
 import filodb.query.exec.binaryOp.BinaryOperatorFunction
 
 /**
