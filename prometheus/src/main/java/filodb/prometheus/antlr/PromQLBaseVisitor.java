7317295c125409f540db5089acefbce09ba8b582
jackson-paul <jackson_paul@apple.com>
2021-08-10 14:14:22 -0700

    fix(prometheus): scala doc compilation issue

--- prometheus/src/main/java/filodb/prometheus/antlr/PromQLBaseVisitor.java
+++ prometheus/src/main/java/filodb/prometheus/antlr/PromQLBaseVisitor.java
@@ -12,276 +12,198 @@ import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
  */
 public class PromQLBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements PromQLVisitor<T> {
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitExpression(PromQLParser.ExpressionContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitBinaryOperation(PromQLParser.BinaryOperationContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitUnaryOperation(PromQLParser.UnaryOperationContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitVectorOperation(PromQLParser.VectorOperationContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitSubqueryOperation(PromQLParser.SubqueryOperationContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitUnaryOp(PromQLParser.UnaryOpContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitPowOp(PromQLParser.PowOpContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitMultOp(PromQLParser.MultOpContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitAddOp(PromQLParser.AddOpContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitCompareOp(PromQLParser.CompareOpContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitAndUnlessOp(PromQLParser.AndUnlessOpContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitOrOp(PromQLParser.OrOpContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitVector(PromQLParser.VectorContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitParens(PromQLParser.ParensContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitInstantOrRangeSelector(PromQLParser.InstantOrRangeSelectorContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitInstantSelector(PromQLParser.InstantSelectorContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitWindow(PromQLParser.WindowContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitOffset(PromQLParser.OffsetContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitSubquery(PromQLParser.SubqueryContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitLabelMatcher(PromQLParser.LabelMatcherContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitLabelMatcherOp(PromQLParser.LabelMatcherOpContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitLabelMatcherList(PromQLParser.LabelMatcherListContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitFunction(PromQLParser.FunctionContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitParameter(PromQLParser.ParameterContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitParameterList(PromQLParser.ParameterListContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitAggregation(PromQLParser.AggregationContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitBy(PromQLParser.ByContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitWithout(PromQLParser.WithoutContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitGrouping(PromQLParser.GroupingContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitOn(PromQLParser.OnContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitIgnoring(PromQLParser.IgnoringContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitGroupLeft(PromQLParser.GroupLeftContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitGroupRight(PromQLParser.GroupRightContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitMetricName(PromQLParser.MetricNameContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitMetricKeyword(PromQLParser.MetricKeywordContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitLabelName(PromQLParser.LabelNameContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitLabelNameList(PromQLParser.LabelNameListContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitLabelKeyword(PromQLParser.LabelKeywordContext ctx) { return visitChildren(ctx); }
 	/**
-	 * {@inheritDoc}
-	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
+	 * [[#visitChildren]] on {@code ctx}.</p>
 	 */
 	@Override public T visitLiteral(PromQLParser.LiteralContext ctx) { return visitChildren(ctx); }
 }
