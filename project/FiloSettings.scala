5f15d285519f7f36684ea5a2c949fd813bc4703a
jackson-paul <jackson_paul@apple.com>
2021-09-15 22:58:37 -0700

    revert: misc(build): do not relocate google.common package

--- project/FiloSettings.scala
+++ project/FiloSettings.scala
@@ -218,7 +218,8 @@ object FiloSettings {
     },
     assemblyShadeRules in assembly := Seq(
       ShadeRule.rename("com.datastax.driver.**" -> "filodb.datastax.driver.@1").inAll,
-      ShadeRule.rename("org.apache.http.**" -> "filodb.org.apache.http.@1").inAll,
+      ShadeRule.rename("com.google.common.**" -> "filodb.com.google.common.@1").inAll,
+      ShadeRule.rename("org.apache.http.**" -> "filodb.org.apache.http.@1").inAll
     ),
     test in assembly := {} //noisy for end-user since the jar is not available and user needs to build the project locally
   )
