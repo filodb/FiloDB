ba10e5a4df840a09fd7404126788d5ef05a26911
Vish Ramachandran <vish.ramachandran@apple.com>
2022-02-09 22:12:57 -0800

    JMH benchmark

--- jmh/src/main/scala/filodb.jmh/PartKeyIndexBenchmark.scala
+++ jmh/src/main/scala/filodb.jmh/PartKeyIndexBenchmark.scala
@@ -1,5 +1,6 @@
 package filodb.jmh
 
+import java.lang.management.{BufferPoolMXBean, ManagementFactory}
 import java.util.concurrent.TimeUnit
 
 import scala.concurrent.duration._
@@ -21,20 +22,21 @@ class PartKeyIndexBenchmark {
 
   org.slf4j.LoggerFactory.getLogger("filodb").asInstanceOf[Logger].setLevel(Level.ERROR)
 
+  println(s"Building Part Keys")
   val ref = DatasetRef("prometheus")
   val partKeyIndex = new PartKeyLuceneIndex(ref, untyped.partition, 0, 1.hour.toMillis)
   val numSeries = 1000000
-  val ingestBuilder = new RecordBuilder(MemFactory.onHeapFactory)
+  val ingestBuilder = new RecordBuilder(MemFactory.onHeapFactory, RecordBuilder.DefaultContainerSize, false)
   val untypedData = TestTimeseriesProducer.timeSeriesData(0, numSeries) take numSeries
   untypedData.foreach(_.addToBuilder(ingestBuilder))
 
-  val partKeyBuilder = new RecordBuilder(MemFactory.onHeapFactory)
+  val partKeyBuilder = new RecordBuilder(MemFactory.onHeapFactory, RecordBuilder.DefaultContainerSize, false)
 
   val converter = new BinaryRegionConsumer {
     def onNext(base: Any, offset: Long): Unit = untyped.comparator.buildPartKeyFromIngest(base, offset, partKeyBuilder)
   }
   // Build part keys from the ingestion records
-  ingestBuilder.allContainers.head.consumeRecords(converter)
+  ingestBuilder.allContainers.foreach(_.consumeRecords(converter))
 
   var partId = 1
   val now = System.currentTimeMillis()
@@ -45,8 +47,20 @@ class PartKeyIndexBenchmark {
       partId += 1
     }
   }
+
+  val start = System.nanoTime()
+  //noinspection ScalaStyle
+  println(s"Indexing started")
   partKeyBuilder.allContainers.foreach(_.consumeRecords(consumer))
   partKeyIndex.refreshReadersBlocking()
+  val end = System.nanoTime()
+  //noinspection ScalaStyle
+  println(s"Indexing finished. Added $partId part keys Took ${(end-start)/1000000000L}s")
+  import scala.collection.JavaConverters._
+
+  //noinspection ScalaStyle
+  println(s"Index Memory Map Size: " +
+    s"${ManagementFactory.getPlatformMXBeans(classOf[BufferPoolMXBean]).asScala.find(_.getName == "mapped").get.getMemoryUsed}")
 
   @Benchmark
   @BenchmarkMode(Array(Mode.Throughput))
@@ -57,7 +71,7 @@ class PartKeyIndexBenchmark {
         Seq(ColumnFilter("_ns_", Filter.Equals(s"App-$i")),
             ColumnFilter("_ws_", Filter.Equals("demo")),
             ColumnFilter("host", Filter.EqualsRegex("H0")),
-            ColumnFilter("__name__", Filter.Equals("heap_usage"))),
+            ColumnFilter("_metric_", Filter.Equals("heap_usage"))),
         now,
         now + 1000)
     }
@@ -66,13 +80,14 @@ class PartKeyIndexBenchmark {
   @Benchmark
   @BenchmarkMode(Array(Mode.Throughput))
   @OutputTimeUnit(TimeUnit.SECONDS)
+  @OperationsPerInvocation(8)
   def emptyPartIdsLookupWithEqualsFilters(): Unit = {
     cforRange ( 0 until 8 ) { i =>
       partKeyIndex.partIdsFromFilters(
         Seq(ColumnFilter("_ns_", Filter.Equals(s"App-${i + 200}")),
           ColumnFilter("_ws_", Filter.Equals("demo")),
           ColumnFilter("host", Filter.EqualsRegex("H0")),
-          ColumnFilter("__name__", Filter.Equals("heap_usage"))),
+          ColumnFilter("_metric_", Filter.Equals("heap_usage"))),
         now,
         now + 1000)
     }
@@ -81,12 +96,13 @@ class PartKeyIndexBenchmark {
   @Benchmark
   @BenchmarkMode(Array(Mode.Throughput))
   @OutputTimeUnit(TimeUnit.SECONDS)
+  @OperationsPerInvocation(8)
   def partIdsLookupWithSuffixRegexFilters(): Unit = {
     cforRange ( 0 until 8 ) { i =>
       partKeyIndex.partIdsFromFilters(
         Seq(ColumnFilter("_ns_", Filter.Equals(s"App-$i")),
           ColumnFilter("_ws_", Filter.Equals("demo")),
-          ColumnFilter("__name__", Filter.Equals("heap_usage")),
+          ColumnFilter("_metric_", Filter.Equals("heap_usage")),
           ColumnFilter("instance", Filter.EqualsRegex("Instance-2.*"))),
         now,
         now + 1000)
@@ -96,12 +112,13 @@ class PartKeyIndexBenchmark {
   @Benchmark
   @BenchmarkMode(Array(Mode.Throughput))
   @OutputTimeUnit(TimeUnit.SECONDS)
+  @OperationsPerInvocation(8)
   def partIdsLookupWithPrefixRegexFilters(): Unit = {
     cforRange ( 0 until 8 ) { i =>
       partKeyIndex.partIdsFromFilters(
         Seq(ColumnFilter("_ns_", Filter.Equals(s"App-$i")),
           ColumnFilter("_ws_", Filter.Equals("demo")),
-          ColumnFilter("__name__", Filter.Equals("heap_usage")),
+          ColumnFilter("_metric_", Filter.Equals("heap_usage")),
           ColumnFilter("instance", Filter.EqualsRegex(".*2"))),
         now,
         now + 1000)
@@ -111,11 +128,24 @@ class PartKeyIndexBenchmark {
   @Benchmark
   @BenchmarkMode(Array(Mode.Throughput))
   @OutputTimeUnit(TimeUnit.SECONDS)
+  @OperationsPerInvocation(8)
   def startTimeLookupWithPartId(): Unit = {
     cforRange ( 0 until 8 ) { i =>
       val pIds = debox.Buffer.empty[Int]
       cforRange ( i * 1000 to i * 1000 + 1000 ) { j => pIds += j }
       partKeyIndex.startTimeFromPartIds(pIds.iterator())
+    }
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.Throughput))
+  @OutputTimeUnit(TimeUnit.SECONDS)
+  @OperationsPerInvocation(8)
+  def getMetricNames(): Unit = {
+    cforRange ( 0 until 8 ) { i =>
+      val filter = Seq(ColumnFilter("_ns_", Filter.Equals(s"App-$i")),
+        ColumnFilter("_ws_", Filter.Equals("demo")))
+      partKeyIndex.shardKeyColValues(filter, now, now + 1000, "_metric_", 10000)
     }
   }
 
