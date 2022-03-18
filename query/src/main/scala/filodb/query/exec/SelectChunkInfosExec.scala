fe9c62680c05c7df48d0c8f6076ed93de00a70bd
Abhinav Khare <abhinav.khare31@gmail.com>
2022-03-18 14:12:53 -0700

    bug(query): handle empty first schema ID in SelectChunkInfosExec (#1355)

--- query/src/main/scala/filodb/query/exec/SelectChunkInfosExec.scala
+++ query/src/main/scala/filodb/query/exec/SelectChunkInfosExec.scala
@@ -2,6 +2,7 @@ package filodb.query.exec
 
 import monix.eval.Task
 import monix.execution.Scheduler
+import monix.reactive.Observable
 
 import filodb.core.DatasetRef
 import filodb.core.memstore.TimeSeriesShard
@@ -47,25 +48,29 @@ final case class SelectChunkInfosExec(queryContext: QueryContext,
     val partMethod = FilteredPartitionScan(ShardSplit(shard), filters)
     val lookupRes = source.lookupPartitions(dataset, partMethod, chunkMethod, querySession)
 
-    val schemas = source.schemas(dataset).get
-    val dataSchema = schema.map { s => schemas.schemas(s) }
-                           .getOrElse(schemas(lookupRes.firstSchemaId.get))
-    val colID = colName.map(n => dataSchema.colIDs(n).get.head).getOrElse(dataSchema.data.valueColumn)
-    val dataColumn = dataSchema.data.columns(colID)
-    val partCols = dataSchema.partitionInfos
-    val numGroups = source.groupsInDataset(dataset)
-    val rvs = source.scanPartitions(dataset, lookupRes, Seq.empty, querySession)
-          .filter(_.hasChunks(chunkMethod))
-          .map { partition =>
-            source.stats.incrReadPartitions(1)
-            val subgroup = TimeSeriesShard.partKeyGroup(dataSchema.partKeySchema, partition.partKeyBase,
-                                                        partition.partKeyOffset, numGroups)
-            val key = PartitionRangeVectorKey(Left(partition),
-                                                  dataSchema.partKeySchema, partCols, shard,
-                                                  subgroup, partition.partID, dataSchema.name)
-            ChunkInfoRangeVector(key, partition, chunkMethod, dataColumn)
-          }
-    ExecResult(rvs, Task.eval(ChunkInfosSchema))
+    if (lookupRes.firstSchemaId.isEmpty) {
+      ExecResult(Observable.empty, Task.eval(ResultSchema.empty))
+    } else {
+      val schemas = source.schemas(dataset).get
+      val dataSchema = schema.map { s => schemas.schemas(s) }
+        .getOrElse(schemas(lookupRes.firstSchemaId.get))
+      val colID = colName.map(n => dataSchema.colIDs(n).get.head).getOrElse(dataSchema.data.valueColumn)
+      val dataColumn = dataSchema.data.columns(colID)
+      val partCols = dataSchema.partitionInfos
+      val numGroups = source.groupsInDataset(dataset)
+      val rvs = source.scanPartitions(dataset, lookupRes, Seq.empty, querySession)
+        .filter(_.hasChunks(chunkMethod))
+        .map { partition =>
+          source.stats.incrReadPartitions(1)
+          val subgroup = TimeSeriesShard.partKeyGroup(dataSchema.partKeySchema, partition.partKeyBase,
+            partition.partKeyOffset, numGroups)
+          val key = PartitionRangeVectorKey(Left(partition),
+            dataSchema.partKeySchema, partCols, shard,
+            subgroup, partition.partID, dataSchema.name)
+          ChunkInfoRangeVector(key, partition, chunkMethod, dataColumn)
+        }
+      ExecResult(rvs, Task.eval(ChunkInfosSchema))
+    }
   }
 
   protected def args: String = s"shard=$shard, chunkMethod=$chunkMethod, filters=$filters, col=$colName"
