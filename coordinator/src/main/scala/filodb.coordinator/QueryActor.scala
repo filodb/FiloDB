9ce35724b94d5376ce55c82d3a19612496d8eb52
Alex Theimer <theimer.alex@gmail.com>
2022-06-22 19:11:29 -0700

    remove ask counters

--- coordinator/src/main/scala/filodb.coordinator/QueryActor.scala
+++ coordinator/src/main/scala/filodb.coordinator/QueryActor.scala
@@ -84,8 +84,6 @@ final class QueryActor(memStore: TimeSeriesStore,
   private val epRequests = Kamon.counter("queryactor-execplan-requests").withTags(TagSet.from(tags))
   private val resultVectors = Kamon.histogram("queryactor-result-num-rvs").withTags(TagSet.from(tags))
   private val queryErrors = Kamon.counter("queryactor-query-errors").withTags(TagSet.from(tags))
-  private val asksReceived = Kamon.counter("queryactor-asks-received").withTags(TagSet.from(tags))
-  private val askResponses = Kamon.counter("queryactor-ask-responses").withTags(TagSet.from(tags))
   private val uncaughtExceptions = Kamon.counter("queryactor-uncaught-exceptions").withTags(TagSet.from(tags))
 
   /**
@@ -127,7 +125,6 @@ final class QueryActor(memStore: TimeSeriesStore,
 
   // scalastyle:off method.length
   def execPhysicalPlan2(q: ExecPlan, replyTo: ActorRef): Unit = {
-    asksReceived.withTags(TagSet.from(Map("receiver" -> this.self.toString()))).increment()
     if (checkTimeout(q.queryContext, replyTo)) {
       epRequests.increment()
       val queryExecuteSpan = Kamon.spanBuilder(s"query-actor-exec-plan-execute-${q.getClass.getSimpleName}")
@@ -145,7 +142,6 @@ final class QueryActor(memStore: TimeSeriesStore,
             FiloSchedulers.assertThreadName(QuerySchedName)
             querySession.close()
             replyTo ! res
-            askResponses.withTags(TagSet.from(Map("sender" -> this.self.toString()))).increment()
             res match {
               case QueryResult(_, _, vectors, _, _, _) => resultVectors.record(vectors.length)
               case e: QueryError =>
@@ -178,7 +174,6 @@ final class QueryActor(memStore: TimeSeriesStore,
               s" query was ${q.queryContext.origQueryParams}", ex)
             queryExecuteSpan.finish()
             replyTo ! QueryError(q.queryContext.queryId, querySession.queryStats, ex)
-            askResponses.withTags(TagSet.from(Map("sender" -> this.self.toString()))).increment()
           }(queryScheduler)
       }
     }
