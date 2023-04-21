f054ae6ea7053c5adb3d0c705e89cc745a44e9fd
Alex Theimer <theimer.alex@gmail.com>
2023-04-21 16:13:11 -0700

    Revert "use raw schema if DS unavailable"
    This reverts commit 0ee3aa77ebdf4725b4a45efb2f0ac4b5eecacc99.

--- core/src/main/scala/filodb.core/memstore/IndexBootstrapper.scala
+++ core/src/main/scala/filodb.core/memstore/IndexBootstrapper.scala
@@ -290,8 +290,8 @@ class DownsampleIndexBootstrapper(colStore: ColumnStore,
       .filter(_.endTime > start)
       .mapParallelUnordered(parallelism) { pk =>
         Task.evalAsync {
-          val schema = schemas(schemaID(pk.partKey, UnsafeUtils.arayOffset))
-          updateDataShapeStatsIfEnabled(pk, schema.downsample.getOrElse(schema), shardNum)
+          val dsSchema = schemas(schemaID(pk.partKey, UnsafeUtils.arayOffset)).downsample.get
+          updateDataShapeStatsIfEnabled(pk, dsSchema, shardNum)
           index.addPartKey(pk.partKey, partId = -1, pk.startTime, pk.endTime)(
             pk.partKey.length,
             PartKeyLuceneIndex.partKeyByteRefToSHA256Digest(pk.partKey, 0, pk.partKey.length)
