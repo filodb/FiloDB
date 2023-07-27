39e4f9b352fe8df53eebb8fe2810e93812f00bc0
Amol Nayak <aa_nayak@apple.com>
2023-07-27 09:19:07 -0700

    fix(query) Regex equals .* must ignore the label and match series even without the label

--- core/src/main/scala/filodb.core/memstore/PartKeyLuceneIndex.scala
+++ core/src/main/scala/filodb.core/memstore/PartKeyLuceneIndex.scala
@@ -822,11 +822,9 @@ class PartKeyLuceneIndex(ref: DatasetRef,
     filter match {
       case EqualsRegex(value) =>
         val regex = removeRegexAnchors(value.toString)
-        if(regex.r.unapplySeq("").isDefined) {
+        if(regex.r.pattern.matcher("").matches()) {
           // Check if the given regex matches the empty string, if yes, then do not consider this label
-          val booleanQuery = new BooleanQuery.Builder
-          val allDocs = new MatchAllDocsQuery
-          booleanQuery.add(allDocs, Occur.FILTER).build()
+          new MatchAllDocsQuery
         } else {
           if (regex.nonEmpty) new RegexpQuery(new Term(column, regex), RegExp.NONE)
           else leafFilter(column, NotEqualsRegex(".+")) // value="" means the label is absent or has an empty value.
