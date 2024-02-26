745eb47e7fa80d71430ce9937360f2f16a93f8f1
alextheimer <theimer.alex@gmail.com>
2024-02-25 21:31:50 -0800

    feat(downsample): support export to multiple paths in Iceberg format (#1720)
    Adds support for export from the downsampler job to multiple destinations in the Iceberg format.
    Support for export of CSV-formatted data is removed.
    
    ---------
    
    Co-authored-by: nikitag55 <nikitagupta551991@gmail.com>

---
+++ spark-jobs/src/main/scala/filodb/downsampler/chunk/ExportConstants.scala
@@ -0,0 +1,21 @@
+package filodb.downsampler.chunk
+
+/**
+ * Iceberg Table columns derived from specific key labels from labels map
+ */
+object ExportConstants {
+  // labels keys in time series
+  val LABEL_NAME = "__name__"
+  val LABEL_METRIC = "_metric_"
+  val LABEL_LE = "le"
+  // column names in iceberg table
+  val COL_LABELS = "labels"
+  val COL_METRIC = "metric"
+  val COL_EPOCH_TIMESTAMP = "epoch_timestamp"
+  val COL_TIMESTAMP = "timestamp"
+  val COL_VALUE = "value"
+  val COL_YEAR = "year"
+  val COL_MONTH = "month"
+  val COL_DAY = "day"
+  val COL_HOUR = "hour"
+}
