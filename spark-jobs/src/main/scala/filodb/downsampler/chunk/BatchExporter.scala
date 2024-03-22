e81d28ac28cbcfea7dcc0db53ead48f0343adfbd
nikitag55 <nikitagupta551991@gmail.com>
2024-03-22 12:41:18 +0530

    Update spark-jobs/src/main/scala/filodb/downsampler/chunk/BatchExporter.scala
    Co-authored-by: alextheimer <theimer.alex@gmail.com>

--- spark-jobs/src/main/scala/filodb/downsampler/chunk/BatchExporter.scala
+++ spark-jobs/src/main/scala/filodb/downsampler/chunk/BatchExporter.scala
@@ -160,10 +160,12 @@ case class BatchExporter(downsamplerSettings: DownsamplerSettings, userStartTime
     val dataSeq = new mutable.ArrayBuffer[Any](exportTableConfig.tableSchema.fields.length)
     // append all dynamic column values
     exportTableConfig.labelColumnMapping.foreach { pair =>
+      // scalastyle: off null
       val result = exportData.labels.get(pair._1) match {
         case Some(labelValue) => labelValue
-        case None => None
+        case None => null
       }
+      // scalastyle: on null
       dataSeq.append(result)
     }
     // append all fixed column values
