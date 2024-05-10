2e4ab60612a86be02a8f0b36b83960f0d0c80edd
Kier Petrov <kpetrov@apple.com>
2024-05-09 22:15:12 -0700

    fix for all shards

--- core/src/main/scala/filodb.core/query/BuddyShardMapper.scala
+++ core/src/main/scala/filodb.core/query/BuddyShardMapper.scala
@@ -3,7 +3,7 @@ package filodb.core.query
 case class ShardInfo(active: Boolean, address: String)
 
 case class ActiveShardMapper(shards: Array[ShardInfo]) {
-  def allShardsActive: Boolean = shards.forall(_.active)
+  def allShardsActive: Boolean = (shards.size > 0) && shards.forall(_.active)
   def activeShards: Seq[Int] = shards.zipWithIndex.filter(_._1.active).map(_._2)
 }
 
