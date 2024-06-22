1b4f0dd94dd874b734691d7db0a885f417235613
Alex Theimer <theimer.alex@gmail.com>
2024-06-21 19:10:52 -0700

    add TargetSchemaProvider

--- core/src/main/scala/filodb.core/TargetSchemaProvider.scala
+++ core/src/main/scala/filodb.core/TargetSchemaProvider.scala
@@ -1,6 +1,7 @@
 package filodb.core
 
-import filodb.core.query.ColumnFilter
+import filodb.core.Utils.ColumnFilterMap
+import filodb.core.query.{ColumnFilter, Filter}
 
 trait TargetSchemaProvider {
   def targetSchemaFunc(filter: Seq[ColumnFilter]): Seq[TargetSchemaChange]
@@ -9,6 +10,44 @@ trait TargetSchemaProvider {
 final case class StaticTargetSchemaProvider(targetSchemaOpt: Option[Seq[String]] = None) extends TargetSchemaProvider {
   def targetSchemaFunc(filter: Seq[ColumnFilter]): Seq[TargetSchemaChange] = {
     targetSchemaOpt.map(tschema => Seq(TargetSchemaChange(0, tschema))).getOrElse(Nil)
+  }
+}
+
+/**
+ * A {@link TargetSchemaProvider} backed by a {@link ColumnFilterMap}.
+ *
+ * @param filterChangesPairs (filters,changes) pairs; at most one of 'filters'
+ *                           can be a non-Equals filter.
+ */
+final class ColumnFilterMapTargetSchemaProvider(filtersChangesPairs: Iterable[(
+                                                 Iterable[ColumnFilter],
+                                                 Iterable[TargetSchemaChange]
+                                               )]) extends TargetSchemaProvider {
+  val columnFilterMap = {
+    val sortedFilterElementPairs = filtersChangesPairs.map { case (filters, changes) =>
+      (filters, changes.toSeq.sortBy(_.time))
+    }
+    new ColumnFilterMap[Seq[TargetSchemaChange]](sortedFilterElementPairs)
+  }
+
+  /**
+   * Given a label->value map, returns an applicable set of
+   *   target-schema changes (if one exists).
+   */
+  def targetSchemaFunc(labelValues: Map[String, String]): Seq[TargetSchemaChange] = {
+    columnFilterMap.get(labelValues).getOrElse(Nil)
+  }
+
+  /**
+   * NOTE!: this implementation only considers {@link Filter.Equals} filters.
+   * All other filters are ignored.
+   */
+  override def targetSchemaFunc(columnFilters: Seq[ColumnFilter]): Seq[TargetSchemaChange] = {
+    val equalsMap = columnFilters
+      .filter(_.filter.isInstanceOf[Filter.Equals])
+      .map(f => f.column -> f.filter.asInstanceOf[Filter.Equals].value.toString)
+      .toMap
+    this.targetSchemaFunc(equalsMap)
   }
 }
 
