47675349c93f3bf3708e151ae07ecf7eced58454
Alex Theimer <theimer.alex@gmail.com>
2024-06-28 15:36:51 -0700

    scalastyle

--- spark-jobs/src/main/scala/filodb/downsampler/chunk/DownsamplerMain.scala
+++ spark-jobs/src/main/scala/filodb/downsampler/chunk/DownsamplerMain.scala
@@ -3,12 +3,15 @@ package filodb.downsampler.chunk
 import java.time.Instant
 import java.time.format.DateTimeFormatter
 import java.util.concurrent.ForkJoinPool
+
 import scala.collection.parallel.ForkJoinTaskSupport
+
 import kamon.Kamon
 import kamon.metric.MeasurementUnit
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+
 import filodb.coordinator.KamonShutdownHook
 import filodb.core.binaryrecord2.RecordSchema
 import filodb.core.memstore.PagedReadablePartition
