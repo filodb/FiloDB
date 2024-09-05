f7182b928be476cd2bc7b04010aceeb39df981de
Vish Ramachandran <vish.ramachandran@gmail.com>
2024-09-05 12:32:16 -0700

    feat(core): Do not index any existing _type_ field since it is a reserved field (#1842)
    Addition of _type_ field to index is now configurable for each cluster, false by default for now.
    Also, if the part-key already has the _type_ field we don't index that since it is a reserved field that we populate.

--- core/src/main/scala/filodb.core/memstore/TimeSeriesShard.scala
+++ core/src/main/scala/filodb.core/memstore/TimeSeriesShard.scala
@@ -285,6 +285,7 @@ class TimeSeriesShard(val ref: DatasetRef,
   private val indexFacetingEnabledAllLabels = filodbConfig.getBoolean("memstore.index-faceting-enabled-for-all-labels")
   private val numParallelFlushes = filodbConfig.getInt("memstore.flush-task-parallelism")
   private val disableIndexCaching = filodbConfig.getBoolean("memstore.disable-index-caching")
+  private val typeFieldIndexingEnabled = filodbConfig.getBoolean("memstore.type-field-indexing-enabled")
 
 
   /////// END CONFIGURATION FIELDS ///////////////////
@@ -313,7 +314,8 @@ class TimeSeriesShard(val ref: DatasetRef,
     */
   private[memstore] final val partKeyIndex: PartKeyIndexRaw = new PartKeyLuceneIndex(ref, schemas.part,
     indexFacetingEnabledAllLabels, indexFacetingEnabledShardKeyLabels, shardNum,
-    storeConfig.diskTTLSeconds * 1000, disableIndexCaching = disableIndexCaching)
+    storeConfig.diskTTLSeconds * 1000, disableIndexCaching = disableIndexCaching,
+    addMetricTypeField = typeFieldIndexingEnabled)
 
   private val cardTracker: CardinalityTracker = initCardTracker()
 
