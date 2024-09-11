7cc9cca819dc68383bfa9dd5d5d4f320e0ccfc35
Alex Theimer <theimer.alex@gmail.com>
2024-09-11 16:34:27 -0700

    Unrevert "Now metadata queries support _type_ filter (#1819)"
    This reverts commit 810808328d685d35c4908faf403900caae32b18c.

--- core/src/main/scala/filodb.core/memstore/PartKeyLuceneIndex.scala
+++ core/src/main/scala/filodb.core/memstore/PartKeyLuceneIndex.scala
@@ -37,10 +37,10 @@ import spire.syntax.cfor._
 
 import filodb.core.{concurrentCache, DatasetRef}
 import filodb.core.Types.PartitionKey
-import filodb.core.binaryrecord2.MapItemConsumer
+import filodb.core.binaryrecord2.{MapItemConsumer, RecordSchema}
 import filodb.core.memstore.ratelimit.CardinalityTracker
+import filodb.core.metadata.{PartitionSchema, Schemas}
 import filodb.core.metadata.Column.ColumnType.{MapColumn, StringColumn}
-import filodb.core.metadata.PartitionSchema
 import filodb.core.query.{ColumnFilter, Filter, QueryUtils}
 import filodb.core.query.Filter._
 import filodb.memory.{BinaryRegionLarge, UTF8StringMedium, UTF8StringShort}
