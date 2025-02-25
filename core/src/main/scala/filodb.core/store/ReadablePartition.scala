0ba878109dbd962640b5d52f7ae196d79e22dcce
sandeep6189 <sandeep6189@gmail.com>
2025-02-25 09:14:12 -0800

    Cherry-picking commits to Integration branch (#1958)
    * bug(core): Type field not returned in multi-label-value query (#1956)
    
    Two fixes:
    When multiple labels are queried in label-value query, and _type_ is included, it would not work
    Downsample Main Spec's exponential histogram unit test was using wrong schema and didnt test min/max
    
    * fix(downsample): Back compatibility of legacy and otel histograms in downsample cluster (#1957)
    
    * adding otel cummulative and delta histogram downsampling unit tests
    
    * Adding unit test for delta + otel-delta mixed mode
    
    ---------
    
    Co-authored-by: Vish Ramachandran <vish.ramachandran@gmail.com>

--- core/src/main/scala/filodb.core/store/ReadablePartition.scala
+++ core/src/main/scala/filodb.core/store/ReadablePartition.scala
@@ -129,21 +129,5 @@ trait ReadablePartition extends FiloPartition {
                           countInfoIterator: CountingChunkInfoIterator): RangeVectorCursor =
     new PartitionTimeRangeReader(this, method.startTime, method.endTime, countInfoIterator, columnIDs)
 
-  /**
-   * @param schemaNameToCheck the schema name to check against
-   * @param schemaHashToCheck the schema hash to check against
-   * @return true if the schema name and hash match or if the schemas are back compatible histograms
-   */
-  final def doesSchemaMatchOrBackCompatibleHistograms(schemaNameToCheck : String, schemaHashToCheck : Int) : Boolean = {
-    if (schema.schemaHash == schemaHashToCheck) { true }
-    else {
-      val sortedSchemas = Seq(schema.name, schemaNameToCheck).sortBy(_.length)
-      val ret = if ((sortedSchemas(0) == "prom-histogram") && (sortedSchemas(1) == "otel-cumulative-histogram")) true
-      else if ((sortedSchemas(0) == "delta-histogram") && (sortedSchemas(1) == "otel-delta-histogram")) true
-      else false
-      ret
-    }
-  }
-
   def publishInterval: Option[Long]
 }
