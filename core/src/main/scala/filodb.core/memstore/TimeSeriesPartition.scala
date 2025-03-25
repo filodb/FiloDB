4b748491550f3875b56ea05f2cd92d89821369fb
Amol Nayak <amolnayak311@gmail.com>
2025-03-24 17:23:35 -0700

    perf(query) Use the last chunk and avoid scan if the data is available in the latest ingesting chunk (#1978)

--- core/src/main/scala/filodb.core/memstore/TimeSeriesPartition.scala
+++ core/src/main/scala/filodb.core/memstore/TimeSeriesPartition.scala
@@ -336,9 +336,16 @@ extends ChunkMap(initMapSize) with ReadablePartition {
   def infos(method: ChunkScanMethod): ChunkInfoIterator = method match {
     case AllChunkScan        => allInfos
     case InMemoryChunkScan   => allInfos
-    case r: TimeRangeChunkScan => allInfos.filter { ic =>
-                                    ic.intersection(r.startTime, r.endTime).isDefined
-                                  }
+    case r: TimeRangeChunkScan =>
+                                     if (currentInfo != nullInfo
+                                      && r.startTime <= r.endTime
+                                      && currentInfo.startTime <= r.startTime) {
+                                       new ElementChunkInfoIterator(chunkmapDoGetLastIterator())
+                                 } else {
+                                    allInfos.filter { ic =>
+                                      ic.intersection(r.startTime, r.endTime).isDefined
+                                    }
+                                }
     case WriteBufferChunkScan => if (currentInfo == nullInfo) ChunkInfoIterator.empty
                                 else {
                                   // Return a single element iterator which holds a shared lock.
