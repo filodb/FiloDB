a7c53608b5213eb60d21abfd8f1c33c30208d429
Vish Ramachandran <vish.ramachandran@gmail.com>
2025-07-17 09:03:40 -0700

    feat(spark-jobs): Label churn finder job can customize startTime for churn

--- spark-jobs/src/main/scala/filodb/labelchurnfinder/LcfTask.scala
+++ spark-jobs/src/main/scala/filodb/labelchurnfinder/LcfTask.scala
@@ -20,22 +20,24 @@ class LcfTask(dsSettings: DownsamplerSettings) extends Serializable with StrictL
 
   @transient lazy private[labelchurnfinder] val logK = 10
   @transient lazy private val schemas = Schemas.fromConfig(dsSettings.filodbConfig).get
-  @transient lazy private[labelchurnfinder] val rawDatasetRef = DatasetRef(dsSettings.rawDatasetName)
+  @transient lazy val datasetName = dsSettings.filodbConfig.as[String]("labelchurnfinder.dataset")
+  @transient lazy val datasetRef = DatasetRef(datasetName)
 
   @transient lazy private val session = DownsamplerContext.getOrCreateCassandraSession(dsSettings.cassandraConfig)
   @transient lazy private[labelchurnfinder] val colStore =
     new CassandraColumnStore(dsSettings.filodbConfig, sched, session, false)(sched)
-  @transient lazy val filters = dsSettings.filodbConfig
+  @transient lazy private[labelchurnfinder] val filters = dsSettings.filodbConfig
     .as[Seq[Map[String, String]]]("labelchurnfinder.pk-filters").map(_.mapValues(_.r.pattern).toSeq)
 
-  @transient lazy val numShards = dsSettings.filodbSettings.streamConfigs
-    .find(_.getString("dataset") == rawDatasetRef.dataset)
+  @transient lazy private[labelchurnfinder] val numShards = dsSettings.filodbSettings.streamConfigs
+    .find(_.getString("dataset") == datasetName)
     .getOrElse(ConfigFactory.empty())
     .as[Option[Int]]("num-shards").get
 
   def computeLabelChurn(split: (String, String),
-                        shard: Int): mutable.HashMap[Seq[String], ChurnSketches] = {
-    colStore.scanPartKeysByStartEndTimeRangeNoAsync(rawDatasetRef, shard, split, 0, Long.MaxValue, 0, Long.MaxValue)
+                        shard: Int,
+                        totalFromTs: Long): mutable.HashMap[Seq[String], ChurnSketches] = {
+    colStore.scanPartKeysByStartEndTimeRangeNoAsync(datasetRef, shard, split, 0, Long.MaxValue, 0, Long.MaxValue)
     .foldLeft(mutable.HashMap.empty[Seq[String], ChurnSketches]) { case (acc, pk) =>
       val rawSchemaId = RecordSchema.schemaID(pk.partKey, UnsafeUtils.arayOffset)
       val schema = schemas(rawSchemaId)
@@ -57,7 +59,7 @@ class LcfTask(dsSettings: DownsamplerSettings) extends Serializable with StrictL
           val key = Seq(ws, ns, pkKey)
           val sketch = acc.getOrElseUpdate(key, ChurnSketches(new CpcSketch(logK), new CpcSketch(logK)))
           val valBytes = pkVal.getBytes
-          sketch.total.update(valBytes)
+          if (pk.endTime > totalFromTs) sketch.total.update(valBytes)
           if (pk.endTime == Long.MaxValue) sketch.active.update(valBytes)
         }
       }
