2a0d7e2d09a26e1784419eac6c3e2905d91510f4
Jay Liu <jayliu@Jays-MBP.home>
2025-08-29 09:10:00 -0700

    remove previous code

--- spark-jobs/src/test/scala/filodb/repair/PartitionKeysCopierSpec.scala
+++ spark-jobs/src/test/scala/filodb/repair/PartitionKeysCopierSpec.scala
@@ -34,9 +34,6 @@ class PartitionKeysCopierSpec extends AnyFunSpec with Matchers with BeforeAndAft
   val sourceConfigPath = "conf/timeseries-filodb-server.conf"
   val targetConfigPath = "spark-jobs/src/test/resources/timeseries-filodb-buddy-server.conf"
 
-//  private val sysConfig = GlobalConfig.systemConfig.getConfig("filodb")
-//  private val sourceConfig = ConfigFactory.parseFile(new File(sourceConfigPath)).getConfig("filodb").withFallback(sysConfig).resolve()
-//  private val targetConfig = ConfigFactory.parseFile(new File(targetConfigPath)).getConfig("filodb").withFallback(sysConfig).resolve()
   private val sysConfig = GlobalConfig.systemConfig
 
   private val sourceConfig = ConfigFactory.parseFile(new File(sourceConfigPath))
