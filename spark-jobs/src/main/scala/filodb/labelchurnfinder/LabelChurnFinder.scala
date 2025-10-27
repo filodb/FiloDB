7370d649828490d024734a58e48b3a9c58e295b7
Vish Ramachandran <vish.ramachandran@gmail.com>
2025-10-27 09:01:27 -0700

    feat(spark-jobs): Use spark sql to calculate label stats

--- spark-jobs/src/main/scala/filodb/labelchurnfinder/LabelChurnFinder.scala
+++ spark-jobs/src/main/scala/filodb/labelchurnfinder/LabelChurnFinder.scala
@@ -1,22 +1,22 @@
 package filodb.labelchurnfinder
 
-import java.time.Instant
-import java.time.format.DateTimeFormatter
-import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.DurationInt
 
-import scala.concurrent.duration.FiniteDuration
-
-import com.typesafe.scalalogging.{Logger, StrictLogging}
-import kamon.Kamon
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.StrictLogging
 import monix.execution.Scheduler
 import net.ceedubs.ficus.Ficus._
+import org.apache.datasketches.hll.HllSketch
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{functions, DataFrame, Dataset, SparkSession}
 import org.apache.spark.sql.functions._
 
-import filodb.cassandra.columnstore.CassandraTokenRangeSplit
+import filodb.cassandra.columnstore.{CassandraColumnStore, CassandraTokenRangeSplit}
+import filodb.core.DatasetRef
+import filodb.core.metadata.Schemas
+import filodb.downsampler.DownsamplerContext
 import filodb.downsampler.chunk.DownsamplerSettings
-import filodb.labelchurnfinder.LcfTask._
+import filodb.memory.format.UnsafeUtils
 
 object LabelChurnFinderMain extends App {
   private val dsSettings = new DownsamplerSettings()
@@ -27,16 +27,30 @@ object LabelChurnFinderMain extends App {
     .appName("LabelChurnFinder")
     .config(sparkConf)
     .getOrCreate()
-  private val result = labelChurnFinder.computeChurn(spark)
-  private val highChurnLabels = labelChurnFinder.computeHighChurnLabels(result)
-  labelChurnFinder.actionOnHighChurnLabels(highChurnLabels)
+  private val labelStats = labelChurnFinder.computeLabelStats(spark)
+  labelChurnFinder.actionOnLabelStats(labelStats)
   spark.stop()
 }
 
-object LCFContext extends StrictLogging {
-  lazy protected[labelchurnfinder] val log: Logger = logger
-  lazy val sched = Scheduler.io("cass-read-sched")
+object LabelChurnFinder {
+  private lazy val sched = Scheduler.io("cass-read-sched")
+  private[labelchurnfinder] val WsCol = "ws"
+  private[labelchurnfinder] val NsGroupCol = "nsGroup"
+  private[labelchurnfinder] val LabelCol = "label"
+  private[labelchurnfinder] val LabelValCol = "labelVal"
+  private[labelchurnfinder] val EndTimeCol = "endTime"
+  private[labelchurnfinder] val LabelSketch1hCol = "labelSketch1h"
+  private[labelchurnfinder] val LabelSketch7dCol = "labelSketch7d"
+  private[labelchurnfinder] val LabelSketch3dCol = "labelSketch3d"
+  private[labelchurnfinder] val Ats1hWithLabelCol = "ats1hWithLabel"
+  private[labelchurnfinder] val Ats3dWithLabelCol = "ats3dWithLabel"
+  private[labelchurnfinder] val Ats7dWithLabelCol = "ats7dWithLabel"
+  private[labelchurnfinder] val LabelCard1h = "labelCard1h"
+  private[labelchurnfinder] val LabelCard3d = "labelCard3d"
+  private[labelchurnfinder] val LabelCard7d = "labelCard7d"
 }
+
+case class LabelValRow(ws: String, ns: String, label: String, labelVal: String, endTime: Long)
 
 /**
  * Requires following typesafe config properties:
@@ -49,100 +63,125 @@ object LCFContext extends StrictLogging {
  *  }
  * ]
  * filodb.labelchurnfinder.dataset = "dataset_name"
- *
- * # one of the two config below is needed
- * filodb.labelchurnfinder.for-time-period = 7 days
- * filodb.labelchurnfinder.since-time  = <timestamp string in iso format>
- *
- * # Thresholds for reporting high churn labels
- * filodb.labelchurnfinder.churn-threshold = 1.5
- *
- * # Thresholds for reporting high churn labels
- * filodb.labelchurnfinder.min-ats-threshold = 1000
  * }}}
  */
-class LabelChurnFinder(dsSettings: DownsamplerSettings) extends Serializable {
+class LabelChurnFinder(dsSettings: DownsamplerSettings) extends Serializable with StrictLogging {
 
-  private val totalFromTs =
-    dsSettings.filodbConfig.as[Option[FiniteDuration]]("labelchurnfinder.for-time-period")
-      .map(dur => System.currentTimeMillis() - dur.toMillis)
-      .orElse(dsSettings.filodbConfig.as[Option[String]]("labelchurnfinder.since-time")
-        .map( str => Instant.from(DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(str)).toEpochMilli))
-      .getOrElse(System.currentTimeMillis() - FiniteDuration(7, TimeUnit.DAYS).toMillis)
+  private val tMinus3d = System.currentTimeMillis() - 3.days.toMillis
+  private val tMinus7d = System.currentTimeMillis() - 7.days.toMillis
 
-  // Read churn threshold from config, default to 1.0 if not set
-  private val churnThreshold = dsSettings.filodbConfig.as[Option[Double]]("labelchurnfinder.churn-threshold")
-                                        .getOrElse(1.5)
+  import LabelChurnFinder._
 
-  // Read min active time series threshold from config,
-  private val minAtsThreshold = dsSettings.filodbConfig.as[Option[Double]]("labelchurnfinder.min-ats-threshold")
-                                        .getOrElse(1000)
+  // lazy since they get initialized and used in executors as well
+  @transient lazy private val session = DownsamplerContext.getOrCreateCassandraSession(dsSettings.cassandraConfig)
+  @transient lazy private[labelchurnfinder] val colStore =
+    new CassandraColumnStore(dsSettings.filodbConfig, sched, session, false)(sched)
+  @transient lazy val datasetName = dsSettings.filodbConfig.as[String]("labelchurnfinder.dataset")
+  @transient lazy val datasetRef = DatasetRef(datasetName)
+  @transient lazy private[labelchurnfinder] val filters = dsSettings.filodbConfig
+    .as[Seq[Map[String, String]]]("labelchurnfinder.pk-filters").map(_.mapValues(_.r.pattern).toSeq)
+
+  @transient private[labelchurnfinder] val numShards = dsSettings.filodbSettings.streamConfigs
+    .find(_.getString("dataset") == datasetName)
+    .getOrElse(ConfigFactory.empty())
+    .as[Option[Int]]("num-shards").get
+
+  /**
+   * Returns iterator of LcfRow objects containing label/value for given token range split and shard.
+   */
+  private def fetchLabelValues(split: (String, String),
+                       shard: Int): Iterator[LabelValRow] = {
+    colStore.scanPartKeysByStartEndTimeRangeNoAsync(datasetRef, shard, split, 0,
+        Long.MaxValue, 0, Long.MaxValue)
+      .flatMap { pk  =>
+        val pkPairs = Schemas.global.part.binSchema.toStringPairs(pk.partKey, UnsafeUtils.arayOffset)
+        val filterMatches = filters.exists { filter => // at least one filter should match
+          filter.forall { case (filterKey, filterValRegex) => // should match all tags in this filter
+            pkPairs.exists { case (pkKey, pkVal) =>
+              pkKey == filterKey && filterValRegex.matcher(pkVal).matches
+            }
+          }
+        }
+        if (filterMatches) {
+          val ws = pkPairs.find(_._1 == "_ws_").map(_._2).getOrElse("unknown!!")
+          val ns = pkPairs.find(_._1 == "_ns_").map(_._2).getOrElse("unknown!!")
+          pkPairs.map { case (label, labelVal) =>
+            LabelValRow(ws, ns, label, labelVal, pk.endTime)
+          }
+        } else Nil
+      }
+  }
 
   /**
    * Returns DataFrame with columns:
-   * WsNsLabel: Array of [ws, ns, labelName]
-   * ActiveCount: Approx count of distinct label values active now (endTime == Long.MaxValue)
-   * TotalCount: Approx count of distinct label values active since totalFromTs
-   * Churn: TotalCount / ActiveCount (0.0 if ActiveCount is 0)
+   * WsCol, NsCol, LabelCol, Ats1hWithLabelCol, LabelCard1hCol, LabelCard3dCol, LabelCard7dCol, Churn7dCol,
+   * Churn3dCol, LabelValueUsageCol
    *
    * The spark session is not closed by this method.
    */
   // scalastyle:off
-  def computeChurn(spark: SparkSession): DataFrame = {
+  def computeLabelStats(spark: SparkSession): DataFrame = {
 
-    val lcfTask = new LcfTask(dsSettings)
+    logger.info(s"This is the Label Churn Finder. Starting job for dataset=$datasetName with filters=$filters")
 
-    LCFContext.log.info(s"This is the Label Churn Finder. Starting job.")
-    LCFContext.log.info(s"Computing churn for dataset=${lcfTask.datasetName}" +
-      s" totalFromTs=${Instant.ofEpochMilli(totalFromTs).toString} churnThreshold=${churnThreshold}" +
-      s" minAtsThreshold=$minAtsThreshold")
-
-    val splitShards = if (lcfTask.colStore.partKeysV2TableEnabled) {
-      spark.sparkContext
-        .parallelize(lcfTask.colStore.getScanSplits(lcfTask.datasetRef))
-        .mapPartitions { split =>
-          Kamon.init() // kamon init should be first thing in worker jvm
-          split.flatMap(_.asInstanceOf[CassandraTokenRangeSplit].tokens)
-        }.map(sp => (sp, -1))
-    } else {
-      spark.sparkContext
-        .parallelize(lcfTask.colStore.getScanSplits(lcfTask.datasetRef))
-        .mapPartitions { split =>
-          Kamon.init() // kamon init should be first thing in worker jvm
-          split.flatMap(_.asInstanceOf[CassandraTokenRangeSplit].tokens)
-            .flatMap { split =>
-              for {shard <- 0 until lcfTask.numShards} yield (split, shard)
-            }
-        }
-    }
-
-    val labels = splitShards.flatMap { case (split, shard) =>
-      LCFContext.log.info(s"Fetching label values iterator for shard=$shard and split=$split")
-      lcfTask.fetchLabelValues(split, shard)
-    }
-
-    lcfTask.computeChurn(spark, labels, totalFromTs)
-  }
-
-  /**
-   * Filters the churn DataFrame to return only those rows which have churn >= churnThreshold
-   */
-  def computeHighChurnLabels(df: DataFrame): DataFrame = {
-    df.filter(col(ChurnColName) > churnThreshold && col(ActiveCountColName) >= minAtsThreshold)
-  }
-
-  /**
-   * Placeholder for future actions on high churn labels like sending alerts, updating aggregation rules etc.
-   */
-  def actionOnHighChurnLabels(df: DataFrame): Unit = {
-    df.withColumn(LabelAndChurnColName, concat_ws("/", col(LabelColName), col(ChurnColName)))
-      .groupBy(WsNsColName)
-      .agg(collect_list(LabelAndChurnColName).as(HCLabelsColName))
-      .foreach { row =>
-        LCFContext.log.info(s"High Churn Label Finder Result " +
-          s"WsNs=${row.getAs[List[String]](WsNsColName).mkString("/")} " +
-          s"HCLabels=${row.getAs[List[String]](HCLabelsColName).mkString(",")} ")
+    val splits = colStore.getScanSplits(datasetRef).flatMap { split =>
+      if (colStore.partKeysV2TableEnabled) {
+        for {
+          t <- split.asInstanceOf[CassandraTokenRangeSplit].tokens
+        } yield (t._1, t._2, -1)
+      } else {
+        for {
+          t <- split.asInstanceOf[CassandraTokenRangeSplit].tokens
+          shard <- 0 until numShards
+        } yield (t._1, t._2, shard)
       }
+    }
+    import spark.implicits._
+    val labelsAndValuesDf = splits
+        .toDF("startToken", "endToken", "shard")
+        .flatMap { split =>
+          fetchLabelValues((split.getString(0), split.getString(1)), split.getInt(2))
+        }
+    computeChurnAndUsage(labelsAndValuesDf, tMinus3d, tMinus7d)
+  }
+
+  /**
+   * Returns DataFrame with columns:
+   * WsCol, NsGroupCol, LabelCol, Ats1hWithLabelCol, LabelSketch1hCol, LabelSketch3dCol, LabelSketch7dCol
+   */
+  private def computeChurnAndUsage(labelAndValuesDf: Dataset[LabelValRow],
+                           tMinus3d: Long,
+                           tMinus7d: Long): DataFrame = {
+
+    val hllSketch = udaf(HllSketchAgg())
+    labelAndValuesDf
+      .groupBy(WsCol, LabelCol)
+      .agg(
+        count(when(col(EndTimeCol) === Long.MaxValue, col(LabelValCol))).alias(Ats1hWithLabelCol),
+        count(when(col(EndTimeCol) > tMinus3d, col(LabelValCol))).alias(Ats3dWithLabelCol),
+        count(when(col(EndTimeCol) > tMinus7d, col(LabelValCol))).alias(Ats7dWithLabelCol),
+        hllSketch(when(col(EndTimeCol) === Long.MaxValue, col(LabelValCol))).alias(LabelSketch1hCol),
+        hllSketch(when(col(EndTimeCol) >= tMinus3d, col(LabelValCol))).alias(LabelSketch3dCol),
+        hllSketch(when(col(EndTimeCol) >= tMinus7d, col(LabelValCol))).alias(LabelSketch7dCol)
+      )
+      .withColumn(NsGroupCol, lit("All")) // placeholder for future ns grouping
+  }
+
+  /**
+   * Placeholder for future actions on label stats - example, sending it to a churn monitoring system etc.
+   */
+  def actionOnLabelStats(df: DataFrame): Unit = {
+    countsFromSketches(df).show(truncate = false)
+  }
+
+  protected[labelchurnfinder] def countsFromSketches(df: DataFrame): DataFrame = {
+    val countUDF = functions.udf { sketch: Array[Byte] =>
+      HllSketch.heapify(sketch).getEstimate
+    }
+    df.withColumn(LabelCard1h, countUDF(col(LabelSketch1hCol)))
+      .withColumn(LabelCard3d, countUDF(col(LabelSketch3dCol)))
+      .withColumn(LabelCard7d, countUDF(col(LabelSketch7dCol)))
+
   }
 
 }
