0f2af2da485515cf953a05c2a68420fe031533ab
Saathvik Viswanathan <saathvikrv@gmail.com>
2025-12-24 20:58:56 -0800

    added implementation for the histogram average row aggregator

---
+++ query/src/main/scala/filodb/query/exec/aggregator/HistAvgRowAggregator.scala
@@ -0,0 +1,60 @@
+package filodb.query.exec.aggregator
+
+import filodb.core.metadata.Column.ColumnType
+import filodb.core.query._
+import filodb.memory.format.RowReader
+
+/**
+  * Map: Every sample is mapped to two values: (a) The value itself (b) and its count value "1"
+  * ReduceAggregate: Accumulator maintains the (a) current mean and (b) sum of counts.
+  *                  Reduction happens by recalculating mean as (mean1*count1 + mean2*count1) / (count1+count2)
+  *                  and count as (count1 + count2)
+  * ReduceMappedRow: Same as ReduceAggregate
+  * Present: The current mean is presented. Count value is dropped from presentation
+  */
+object HistAvgRowAggregator extends RowAggregator {
+  class HistAvgHolder(var timestamp: Long = 0L,
+                      var mean: Double = Double.NaN,
+                      var count: Long = 0) extends AggregateHolder {
+    val row = new AvgAggTransientRow()
+    def toRowReader: MutableRowReader = {
+      row.setLong(0, timestamp)
+      row.setDouble(1, mean)
+      row.setLong(2, count)
+      row
+    }
+    def resetToZero(): Unit = { count = 0; mean = Double.NaN }
+  }
+  type AggHolderType = HistAvgHolder
+  def zero: HistAvgHolder = new HistAvgHolder()
+  def newRowToMapInto: MutableRowReader = new AvgAggTransientRow()
+  def map(rvk: RangeVectorKey, item: RowReader, mapInto: MutableRowReader): RowReader = {
+    val count = item.getDouble(2)
+    val sum = item.getDouble(1)
+
+    mapInto.setLong(0, item.getLong(0))
+    mapInto.setDouble(1, if (count.isNaN || count == 0) 0 else sum/count)
+    mapInto.setDouble(2, count)
+    mapInto
+  }
+  def reduceAggregate(acc: HistAvgHolder, aggRes: RowReader): HistAvgHolder = {
+    acc.timestamp = aggRes.getLong(0)
+    if (!aggRes.getDouble(1).isNaN) {
+      if (acc.mean.isNaN) acc.mean = 0d
+      val newMean = (acc.mean * acc.count + aggRes.getDouble(1) * aggRes.getLong(2)) / (acc.count + aggRes.getLong(2))
+      acc.mean = newMean
+      acc.count += aggRes.getLong(2)
+    }
+    acc
+  }
+  // ignore last count column. we rely on schema change
+  def present(aggRangeVector: RangeVector, limit: Int, rangeParams: RangeParams,
+              queryStats: QueryStats): Seq[RangeVector] = Seq(aggRangeVector)
+  def reductionSchema(source: ResultSchema): ResultSchema = {
+    source.copy(columns = source.columns :+ ColumnInfo("count", ColumnType.LongColumn))
+  }
+  def presentationSchema(reductionSchema: ResultSchema): ResultSchema = {
+    // drop last column with count
+    reductionSchema.copy(reductionSchema.columns.filterNot(_.name.equals("count")))
+  }
+}
