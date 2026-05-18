8a9fa90a2b357a3e5905a36a2bd01db1b8086427
yu-shipit <120689245+yu-shipit@users.noreply.github.com>
2026-05-18 15:35:18 -0700

    revert ds changes. (#2241)
    * Revert "feat(downsample): Allowing to persist downsampled data using arbitrary implementation of ChunkPersistor and DSPartitionReader (#2034)"
    
    This reverts commit 598fb4bc0d4bf5204f06840cbf09fe15e6755eea.
    
    * version.
    
    ---------
    
    Co-authored-by: Yu Zhang <yzhang999272@apple.com>

--- cassandra/src/main/scala/filodb.cassandra/columnstore/IngestionTimeIndexTable.scala
+++ cassandra/src/main/scala/filodb.cassandra/columnstore/IngestionTimeIndexTable.scala
@@ -38,7 +38,7 @@ sealed class IngestionTimeIndexTable(val dataset: DatasetRef,
                      |) WITH compression = {
                     'sstable_compression': '$sstableCompression'}""".stripMargin
 
-  lazy val writeIndexCql = session.prepare(
+  private lazy val writeIndexCql = session.prepare(
     s"INSERT INTO $tableString (partition, ingestion_time, start_time, info) " +
     s"VALUES (?, ?, ?, ?) USING TTL ?")
     .setConsistencyLevel(writeConsistencyLevel)
